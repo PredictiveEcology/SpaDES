5f23c6c77eb3b3e1d358299a0f46ab50c63963c0
eliotmcintire <eliotmcintire@gmail.com>
2014-02-11 15:48:51 -0800

    Created initialization function
    Specifically, for starting a specific number of agents in a specific set
    of patches

--- R/ABM_code_files.R
+++ R/ABM_code_files.R
@@ -36,8 +36,16 @@ setMethod("initialize", "agent", function(.Object, agentlocation = NULL, numagen
     numagents = length(pos)
 
   } else {
-    pos = SpatialPoints(sampleRandom(al, na, xy = T, sp = T))
-    last.pos = SpatialPoints(sampleRandom(al, na, xy = T, sp = T))
+    # probinit is NULL - start exactly the number of agents as there
+    # are pixels in agentlocation
+    if (!is.null(numagents)) {
+        pos = SpatialPoints(sampleRandom(agentlocation, numagents, xy = T, sp = T))
+        last.pos = SpatialPoints(sampleRandom(agentlocation, numagents, xy = T, sp = T))
+    } else { # for numagents also NULL
+        pos = SpatialPoints(xyFromCell(agentlocation,Which(agentlocation,cells=T)))
+        last.pos = SpatialPoints(xyFromCell(agentlocation,Which(agentlocation,cells=T)))
+        numagents = length(pos)
+    }
   }
 #  heading = deg(atan((pos@coords[,"x"] - last.pos@coords[,"x"]) / (pos@coords[,"y"] - last.pos@coords[,"y"])))
 #    heading = ifelse((pos@coords[,"y"] - last.pos@coords[,"y"])<0,
@@ -145,7 +153,7 @@ setMethod("coordinates", signature = "agent",
   
 setGeneric("agent", function(object) standardGeneric("agent"))
  
-ProbInit = function(map,p,absolute=F) { # currently, there is no ability to have Absolute ProbInit
+ProbInit = function(map,p,absolute=F) { #
   if (length(p) == 1) { 
     ProbInit = raster(extent(map),nrows=nrow(map),ncols=ncol(map),crs = crs(map))
     ProbInit = setValues(ProbInit, rep(p,length(ProbInit)))
@@ -293,3 +301,39 @@ GaussMap = function(ext, cov.pars = c(5,100), speedup.index = 10){#, fast = T, n
   if(speedup.index>1) map3 <- disaggregate(map2,c(xfact,yfact))
   return(map3)
 }
+
+# To initialize with a specific number per patch, which may come from
+#  data or have been derived from patch size. Options include a combination of either
+#  a patchid map and a table with 2 columns, pops and num.in.pop,
+#  or 2 maps, patchid and patchnumber. Returns a map with a single unique pixel
+#  within each patch representing an agent to start. This means that the number
+#  of pixels per patch must be greater than the number of agents per patch
+spec.num.per.patch = function(patches, num.per.patch.table = NULL, num.per.patch.map=NULL) {
+  patchids = as.numeric(na.omit(getValues(patches)))
+  wh = Which(patches, cells = T)
+  if (!is.null(num.per.patch.table)) {
+    dt1 = data.table(wh,pops = patchids)
+    setkey(dt1,pops)
+    if (any(class(num.by.patch.table)!="data.table")) num.by.patch.table = data.table(num.by.patch.table)
+    setkey(num.by.patch.table,pops)
+    dt2 = dt1[num.per.patch.table]
+  } else if (!is.null(num.per.patch.map)) {
+    num.per.patch.table = as.numeric(na.omit(getValues(num.per.patch.map)))
+    dt2 = data.table(wh,pops = patchids, num.in.pop = num.per.patch.table)
+  }
+
+  dt3 = dt2[,list(cells=sample(wh,unique(num.in.pop))),by=pops]
+  dt3$ids = rownames(dt3)
+
+  al = raster(patches)
+  al[dt3$cells] = 1
+
+  return(al)
+}
+
+
+patch.size = function(patches) {
+  patch.size = freq(patches)
+  return(patch.size)
+}
+
