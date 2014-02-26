6b320fab49911f5b331957e7719f308e998d0931
Alex Chubaty <alex.chubaty@gmail.com>
2014-02-26 15:06:19 -0800

    cleaned up and modularized DES code
    1. cleaned up and commented discrete event simulator
    2. modularized things to easily add/remove submodels

--- R/DES-ABM.R
+++ R/DES-ABM.R
@@ -1,76 +1,100 @@
-# DES-ABM.R:  R routines for discrete-event simulation (DES)
-# modified from Matloff 2009 to use data.table instead of data.frame
+#######################################################################
+###     DES-ABM.R:  R routines for discrete-event simulation (DES)  ###
+###                                                                 ###
+###     Modified from Matloff (2009):                               ###
+###     - uses `data.table` instead of `data.frame`                 ###
+###     - implemented in a more modular fashion so it's easier      ###
+###       to add submodules to the simulation                       ###
+#######################################################################
 
 require(data.table)
 
-# each event will be represented by a data table row consisting of the
-# following components:
-#   evnttime, the time the event is to occur;
-#   evnttype, a character string for the programmer-defined event type;
-#   optional application-specific components,
-#       e.g., the job's arrival time in a queuing app
+##  OVERVIEW:
+##
+##  A global list named "sim" holds:
+##       events: the events data.table;
+##       currtime: the current simulated time;
+##       debug: indicates debugging mode
+##
+##  Each event is represented by a data.table row consisting of:
+##      event.time: the time the event is to occur;
+##      module.name: the module from which the event is taken;
+##      event.type: a character string for the programmer-defined event type;
+##      : optional application-specific components.
+##
 
-# a global list named "sim" holds the events data frame, evnts, and
-# current simulated time, currtime; there is also a component dbg, which
-# indicates debugging mode
 
-# insert event with time evnttm and type evntty into event list;
-# appin is an optional set of application-specific traits of this event,
+
+# insert event with time `time.event` and type `type.event` into event list;
+# other.info is an optional set of application-specific traits of this event,
 # specified in the form a list with named components
-schedevnt <- function(evnttm, evntty, appin=NULL) {
-    # forms a row for an event of type evntty that will occur at time evnttm;
-    # see comments in schedevnt() regarding appin
-    newevnt <- as.data.table(c(list(evnttime=evnttm,evnttype=evntty),appin))
-    setkey(newevnt, evnttime)
-   # if the event list is empty, set it to consist of evnt and return
-   if (length(sim$evnts)==0) {
-      sim$evnts <<- newevnt
-      return()
-   }
-   # otherwise, "insert," by reconstructing the data frame; we find what
-   # portion of the current matrix should come before the new event and
-   # what portion should come after it, then string everything together
-   before <- sim$evnts[evnttime<newevnt$evnttime[1]]
-   after <- sim$evnts[evnttime>newevnt$evnttime[1]]
-   sim$evnts <<- setkey(rbind(before,newevnt,after), evnttime)
+schedule.event <- function(event.time, module.name, event.type, other.info=NULL) {
+    # forms a row for an event of type `type.event` from the module `name.module`
+    #  that will occur at time `time.event`;
+    # see comments in `schedule.event()` regarding `other.info`
+    new.event <- as.data.table(c(list(event.time=event.time,
+                                      module.name=module.name,
+                                      event.type=event.type),
+                                 other.info)) # `other.info` should be a named list
+    setkey(new.event, event.time)
+    
+    # if the event list is empty, set it to consist of evnt and return
+    if (length(sim$events)==0) {
+        sim$events <<- new.event
+        return()
+    }
+    
+    # otherwise, "insert" by reconstructing the data frame;
+    # find what portion of the current matrix should come before the new event,
+    # and what portion should come after it, then bind everything together.
+    before <- sim$events[event.time<new.event$event.time[1]]
+    after <- sim$events[event.time>new.event$event.time[1]]
+    sim$events <<- setkey(rbind(before,new.event,after), event.time)
 }
 
-# start to process next event (second half done by application
-# programmer via call to reactevnt()) 
-getnextevnt <- function() {
-   head <- sim$evnts[1,]
-   # delete head
-   if (nrow(sim$evnts) == 1) {
-      sim$evnts <<- NULL
-   } else sim$evnts <<- sim$evnts[-1,]
-   return(head)
+# start to process next event;
+#  second half done by application programmer via call to `react.event()`
+get.next.event <- function() {
+    head <- sim$events[1,]
+    # delete head
+    if (nrow(sim$events) == 1) {
+        sim$events <<- NULL
+    } else sim$events <<- sim$events[-1,]
+    return(head)
 }
 
-# simulation body
-# arguments:
-#    initglbls:  application-specific initialization function; inits
-#      globals to statistical totals for the app, etc.; records apppars
-#      in globals; schedules the first event
-#    reactevnt: application-specific event handling function, coding the
-#       proper action for each type of event
-#    prntrslts:  prints application-specific results, e.g. mean queue
-#       wait
-#    apppars:  list of application-specific parameters, e.g.
-#      number of servers in a queuing app
-#    maxsimtime:  simulation will be run until this simulated time 
-#    dbg:  debug flag; if TRUE, sim will be printed after each event
-dosim <- function(initglbls,reactevnt,prntrslts,maxsimtime,apppars=NULL,dbg=FALSE) {
-   sim <<- list()
-   sim$currtime <<- 0.0  # current simulated time
-   sim$evnts <<- NULL  # events data table (filled in by schedevnt)
-   sim$dbg <<- dbg
-   
-   initglbls(apppars)
-   while(sim$currtime < maxsimtime) {  
-      head <- getnextevnt()
-      sim$currtime <<- head$evnttime  # update current simulated time
-      reactevnt(head)  # process this event 
-      if (dbg) print(sim)
-   }
-   prntrslts()
+# simulation body, takes the following arguments:
+#   globals.init:  application-specific initialization function;
+#                   inits globals to statistical totals for the app, etc.;
+#                   records params in globals;
+#                   schedules the first event.
+#   react.event:   application-specific event handling function, coding the
+#                   proper action for each type of event.
+#   print.results: prints application-specific results.
+#   params:        list of application-specific parameters.
+#   modules:       list of module names used in the simulation.
+#   maxsimtime:    simulation will be run until this simulated time.
+#   debug:         logical flag determines whether sim debug info will be printed.
+dosim <- function(globals.init, react.event, print.results, maxsimtime, params=NULL, modules=NULL, debug=FALSE) {
+    sim <<- list()
+    sim$currtime <<- 0.0  # current simulated time
+    sim$events <<- NULL  # events data table (filled in by `schedule.event()`)
+    sim$debug <<- debug
+    
+    globals.init(params)
+    while(sim$currtime < maxsimtime) {  
+        head <- get.next.event()
+        sim$currtime <<- head$event.time  # update current simulated time
+        react.event(head)  # process this event 
+        
+        # print debugging info
+        #  this can, and should, be more sophisticated;
+        #  i.e., don't simply print the enitre object
+        if (debug) {
+            print(sim)
+        }
+    }
+    
+    # print simulation results
+    print.results(modules, debug)
 }
