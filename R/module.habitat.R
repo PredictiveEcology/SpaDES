12e6799ec348c23663c3c431c8820cec224aa23c
Alex Chubaty <alex.chubaty@gmail.com>
2014-03-14 14:06:31 -0700

    moved module-specific elements out of global
    moved module-specific elements out of global and into the module

--- R/module.habitat.R
+++ R/module.habitat.R
@@ -1,7 +1,64 @@
-### list required packages here
-pkgs = list("raster")
-
 ### load any required packages
+pkgs = list("raster") # list required packages here
 load.required.pkgs(pkgs)
 
-###
+### module parameters
+#   - export params to global list `abm.globals$params`;
+abm.globals$params[['MODULE.NAME']] <<- list()
+
+#   - instantiate data structure for module stats
+abm.globals$modulestats[['MODULE.NAME']] <<- list()
+
+### event functions:
+#   - follow the naming convention `moduleName.eventType()`;
+#   - `moduleName.init()` function is required for initiliazation;
+#   - keep event functions short and clean, modularize by calling
+#       subroutines from section below.
+habitat.react.event = function(event.time, event.type)
+    if (event.type=="init") {
+        # do stuff for this event
+
+        
+        # schedule the next event
+        schedule.event(TIME, "MODULE.NAME", "EVENT.TYPE")
+    } else {
+        # do stuff for this event
+        print("polar bears. grr!")
+        
+        # schedule the next event
+        schedule.event(TIME, "MODULE.NAME", "EVENT.TYPE")
+    }
+    
+    
+#     if (head$evnttype == "arrv") {  # arrival
+#       # if server free, start service, else add to queue (added to queue
+#       # even if empty, for convenience)
+#       if (length(abm.globals$srvq) == 0) {
+#          abm.globals$srvq <<- head$arrvtime
+#          srvdonetime <- sim$currtime + rexp(1,abm.globals$srvrate)
+#          schedule.event(srvdonetime,"srvdone",list(arrvtime=head$arrvtime))
+#       } else abm.globals$srvq <<- c(abm.globals$srvq,head$arrvtime)
+#       # generate next arrival
+#       arrvtime <- sim$currtime + rexp(1,abm.globals$arrvrate)
+#       schedule.event(arrvtime,"arrv",list(arrvtime=arrvtime))
+#    } else {  # service done
+#       # process job that just finished
+#       # do accounting
+#       abm.globals$njobsdone <<- abm.globals$njobsdone + 1
+#       abm.globals$totwait <<- 
+#          abm.globals$totwait + sim$currtime - head$arrvtime
+#       # remove from queue
+#       abm.globals$srvq <<- abm.globals$srvq[-1]
+#       # more still in the queue?
+#       if (length(abm.globals$srvq) > 0) {
+#          # schedule new service
+#          srvdonetime <- sim$currtime + rexp(1,abm.globals$srvrate)
+#          schedule.event(srvdonetime,"srvdone",list(arrvtime=abm.globals$srvq[1]))
+#       }
+#    }
+
+
+
+
+
+### user-defined subroutines
--- R/simulation-abm.R
+++ R/simulation-abm.R
@@ -12,19 +12,15 @@ abm.globals.init <- function(params, modules) {
    abm.globals <<- list()
    
    # simulation parameters (global)
-   abm.globals$param1 <<- params$param1 # do this for each parameter
+   abm.globals$params <<- params
    
-   # simulation modules
+   # load simulation modules
    abm.globals$modules <<- modules # this should be a list of module names
-   for (m in modules) source(paste(m, ".R", sep="")) # load each module from file
-   
-   # other simulation parameters (e.g., from modules)
-   abm.globals$module1.param1 <<- params$param1 # do this for each module and each parameter
+   for (m in modules) source(paste("module.", m, ".R", sep="")) # source each module from file
    
    # statistics
-   #    some will be "hard coded" here as global stats, but most will be module-specific
+   #    some will be "hard coded" here as global stats (e.g., execution time)
    abm.globals$globalstats <<- list()   # name and init these accordingly
-   abm.globals$modulestats <<- list()   # name and init these accordingly for each module
    
    # set up first event(s): all first events should be initialization events e.g. from modules
    #    schedule.event(EVENT.TIME, "MODULE.NAME", "EVENT.TYPE", list(OPTIONAL.ITEMS))
@@ -43,37 +39,12 @@ abm.react.event <- function(head) {
     # having to worry about updating this (hardcoded) list.
     
     module.call <- paste(head$module.name, "react.event", sep=".")
+    check.validity(module.call) # do it here, otherwise user must do it
+                                # per module in the react.event function?
     get(module.call)(head$event.time, head$event.type)
     
     # e.g., this would produce the following call to the fire module:
     #   module.fire.react.event(TIME, "TYPE")
-
-#     if (head$evnttype == "arrv") {  # arrival
-#       # if server free, start service, else add to queue (added to queue
-#       # even if empty, for convenience)
-#       if (length(abm.globals$srvq) == 0) {
-#          abm.globals$srvq <<- head$arrvtime
-#          srvdonetime <- sim$currtime + rexp(1,abm.globals$srvrate)
-#          schedule.event(srvdonetime,"srvdone",list(arrvtime=head$arrvtime))
-#       } else abm.globals$srvq <<- c(abm.globals$srvq,head$arrvtime)
-#       # generate next arrival
-#       arrvtime <- sim$currtime + rexp(1,abm.globals$arrvrate)
-#       schedule.event(arrvtime,"arrv",list(arrvtime=arrvtime))
-#    } else {  # service done
-#       # process job that just finished
-#       # do accounting
-#       abm.globals$njobsdone <<- abm.globals$njobsdone + 1
-#       abm.globals$totwait <<- 
-#          abm.globals$totwait + sim$currtime - head$arrvtime
-#       # remove from queue
-#       abm.globals$srvq <<- abm.globals$srvq[-1]
-#       # more still in the queue?
-#       if (length(abm.globals$srvq) > 0) {
-#          # schedule new service
-#          srvdonetime <- sim$currtime + rexp(1,abm.globals$srvrate)
-#          schedule.event(srvdonetime,"srvdone",list(arrvtime=abm.globals$srvq[1]))
-#       }
-#    }
 }
 
 abm.print.results <- function(modules, debug) {
