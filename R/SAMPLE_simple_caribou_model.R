0194300c98319fe523ed0b7fba5ab237fb85eacc
Alex Chubaty <alex.chubaty@gmail.com>
2014-03-18 16:41:56 -0700

    updated default path in sample caribou simulation

--- R/SAMPLE_simple_caribou_model.R
+++ R/SAMPLE_simple_caribou_model.R
@@ -1,4 +1,4 @@
-setwd("/Users/achubaty/Dropbox/POSTDOCS/CFS-MPB-2014/r-code/ABM/R")
+setwd("~/GitHub/ABM/R")
 source("ABM_code_files.R")
 source("simulation.R")
 dosim(globals.init, do.event, print.results, maxsimtime=10.00, modules=list("habitat", "caribou"))
