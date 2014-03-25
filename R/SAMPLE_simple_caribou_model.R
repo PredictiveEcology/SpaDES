0ce6a88850193ecdb66d9382f7c4f44b057b9098
Alex Chubaty <alex.chubaty@gmail.com>
2014-03-25 09:54:16 -0700

    added stuff for code profiling of memory usage
    added `lineprof` and `pryr` to profile memory usage as per http://adv-r.had.co.nz/memory.html

--- R/SAMPLE_simple_caribou_model.R
+++ R/SAMPLE_simple_caribou_model.R
@@ -1,4 +1,22 @@
+### install neccesarry packages to profile development code
+install.packages("devtools")
+#   Windows also needs 'Rtools' from
+#   http://cran.r-project.org/bin/windows/Rtools/index.html
+#   (this is a seperate install of an .exe file)
+
+devtools::install_github("lineprof")
+library(lineprof)
+
+devtools::install_github("pryr")
+library(pryr)
+
+## simulation code
 setwd("~/GitHub/ABM/R")
 source("ABM_code_files.R")
 source("simulation.R")
 dosim(sim.init, do.event, print.results, maxsimtime=10.00, modules=list("habitat", "caribou"))
+
+## profiling of development code
+prof <- lineprof(dosim(sim.init, do.event, print.results, maxsimtime=10.00, modules=list("habitat", "caribou")))
+shine(prof)
+c(address(sim), refs(sim))
