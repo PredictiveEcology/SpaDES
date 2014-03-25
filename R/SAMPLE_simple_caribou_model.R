44a0d92395675119fc74495d4fa5d584a2b6eaf8
Alex Chubaty <alex.chubaty@gmail.com>
2014-03-25 10:01:18 -0700

    updated setwd path

--- R/SAMPLE_simple_caribou_model.R
+++ R/SAMPLE_simple_caribou_model.R
@@ -11,7 +11,7 @@ devtools::install_github("pryr")
 library(pryr)
 
 ## simulation code
-setwd("~/GitHub/ABM/R")
+setwd("~/Documents/GitHub/ABM/R")
 source("ABM_code_files.R")
 source("simulation.R")
 dosim(sim.init, do.event, print.results, maxsimtime=10.00, modules=list("habitat", "caribou"))
