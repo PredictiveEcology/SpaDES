bda1302cf190388c06e0616a5f722056b543eea3
Alex Chubaty <alex.chubaty@gmail.com>
2014-03-25 16:59:54 -0700

    moved sample modules to SAMPLE directory
    updated sample caribou model to use devtools package install
    moved sample modules to SAMPLE directory
    (this mean dosim() won't work because it won't find the sample modules)

--- SAMPLE/SAMPLE_simple_caribou_model.R
+++ SAMPLE/SAMPLE_simple_caribou_model.R
@@ -1,25 +1,24 @@
-### install neccesarry packages to profile development code
+### set the working directory
+setwd("~/GitHub/")  #setwd("~/Documents/GitHub/")
 
+### development only:
 install.packages("devtools", dependencies=TRUE)
 #   Windows also needs 'Rtools' from
 #   http://cran.r-project.org/bin/windows/Rtools/index.html
 #   (this is a seperate install of an .exe file)
-
 devtools::install_github("lineprof")
 devtools::install_github("pryr")
-
 library(lineprof)
 library(pryr)
 
-## simulation code
+### load ABM package
+devtools::load_all("ABM") # for development/testing
+#devtools::install_github("ABM", username="achubaty")   # local install from GitHub
+#library(ABM)   # local installation from CRAN
 
-setwd("~/GitHub/ABM/R")
-#setwd("~/Documents/GitHub/ABM/R")
-source("ABM_code_files.R")
-source("maps.R")
-source("movement.R")
-source("simulation.R")
+## simulation code
 dosim(sim.init, do.event, print.results, maxsimtime=10.00, modules=list("habitat", "caribou"))
+# THIS IS BROKEN RIGHT NOW BECAUSE I MOVED THE SAMPLE MODULES TO A DIFF DIR
 
 ## profiling of development code
 prof <- lineprof(dosim(sim.init, do.event, print.results, maxsimtime=10.00, modules=list("habitat", "caribou")))
