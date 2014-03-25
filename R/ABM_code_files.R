93df76c3b886d932d4e5c185a74eda727abee3dc
Alex Chubaty <alex.chubaty@gmail.com>
2014-03-25 14:02:58 -0700

    updates to agent classes and plotting
    added new agent class: rasterAgent (inherits agent)
    added new agent class: vectorAgent (inherits agent)
    added new agent class: polygonAgent (inherits vectorAgent)
    changed spatialAgent to pointAgent  (inherits vectorAgent)
    changed spreadAgent to inherit rasterAgent
    renamed "position" slot to "spatial"
    created new file, plotting.R, for our library of plot functions
    moved plotting functions to separate file, plotting.R

--- R/ABM_code_files.R
+++ R/ABM_code_files.R
@@ -106,13 +106,30 @@ setMethod("setOther",
 
 
 
+### rasterAgent class extends agent by making it spatial
+setClass("rasterAgent", slots=list(), contains="agent") # need init etc methods
 
-### spatialAgent class extends agent by making it spatial
-setClass("spatialAgent", slots=list(position="SpatialPoints"), contains="agent")
+
+
+
+### vectorAgent class extends agent by making it spatial
+setClass("vectorAgent", slots=list(), contains="agent") # need init etc methods
+
+
+
+
+### polygonAgent class extends agent by making it spatial
+setClass("polygonAgent", slots=list(spatial="SpatialPolygon"), contains="vectorAgent") # need init etc methods
+
+
+
+
+### pointAgent class extends vectorAgent
+setClass("pointAgent", slots=list(spatial="SpatialPoints"), contains="vectorAgent")
 
 # define methods that extend already-prototyped functions in R
 setMethod("initialize",
-          signature = "spatialAgent",
+          signature = "pointAgent",
           definition = function(.Object, numagents=NULL, ...) {
               # init agent IDs as integer increments by default
               #  unless specified by user;
@@ -120,39 +137,38 @@ setMethod("initialize",
               if (!is.null(numagents)) {
                   .Object@ID = as.character(1:numagents)
                   # temporarily assign random positions
-                  tmp <- SpatialPoints(cbind(x=runif(n=numagents, min=-1000, max=1000),
-                                             y=runif(n=numagents, min=-1000, max=1000)))
-                  .Object@position = tmp
+#                  tmp <- SpatialPoints(cbind(x=runif(n=numagents, min=-1000, max=1000),
+#                                             y=runif(n=numagents, min=-1000, max=1000)))
+#                  .object@spatial = tmp
               }
               return(.Object)
 })
 
 setMethod("show",
-          signature = "spatialAgent",
+          signature = "pointAgent",
           definition = function(object) {
               show = list()
               show[["N"]] = paste("There are", length(object@ID), "agents")
               show[["First 5 agent IDs"]] = head(object@ID, 5)
-              show[["First 5 agent coordinates"]] = head(coordinates(object@position), 5)
+              show[["First 5 agent coordinates"]] = head(coordinates(object@spatial), 5)
               # needs to print `other`
               print(show)
 })
 
 setMethod("coordinates",
-          signature = "spatialAgent",
+          signature = "pointAgent",
           definition = function(obj, ...) {
               coords <- coordinates(position(obj), ...)
               return(coords)
 })
 
 setMethod("points",
-          signature = "spatialAgent",
+          signature = "pointAgent",
           definition = function(x, which.to.plot=NULL, ...) {
               if (is.null(which.to.plot)) { sam = 1:length(x)} else {sam = which.to.plot}
               points(x@position@coords[sam,], ...)
 })
 
-#setGeneric("spatialAgent", function(object) standardGeneric("spatialAgent")) # remove?
 
 
 # define our custom methods, which need to be prototyped
@@ -161,55 +177,25 @@ setGeneric("position", function(obj, ...) {
 })
 
 setMethod("position",
-          signature = "spatialAgent",
+          signature = "pointAgent",
           definition = function(obj, ...) {
-              return(obj@position)
+              return(obj@spatial)
 })
 
 
 
 
-### spreadAgent class extends spatialAgent by not only storing single position but also area
-setClass("spreadAgent", slots=list(NumPixels="numeric"), prototype=list(NumPixels=NA_integer_), contains="spatialAgent")
+### spreadAgent class extends pointAgent by not only storing single position but also area
+setClass("spreadAgent", slots=list(NumPixels="numeric"),
+         prototype=list(NumPixels=NA_integer_), contains="rasterAgent")
 
-# define methods that extend already-prototyped functions in R
-setMethod("initialize",
-          signature = "spreadAgent",
-          definition = function(.Object, numagents=NULL, ...) {
-              # init agent IDs as integer increments by default
-              #  unless specified by user;
-              # init positions;
-              # init NumPixels.
-              if (!is.null(numagents)) {
-                  .Object@ID = as.character(1:numagents)
-                  # temporarily assign random positions
-                  tmp <- SpatialPoints(cbind(x=runif(n=numagents, min=-1000, max=1000),
-                                             y=runif(n=numagents, min=-1000, max=1000)))
-                  .Object@position = tmp
-                  # temporarily init random NumPixels
-                  tmp <- sample(1:10, replace=TRUE)
-                  .Object@NumPixels = tmp
-              }
-              return(.Object)
-})
-
-setMethod("show",
-          signature = "spreadAgent",
-          definition = function(object) {
-              show = list()
-              show[["N"]] = paste("There are", length(object@ID), "agents")
-              show[["First 5 agent IDs"]] = head(object@ID, 5)
-              show[["First 5 agent coordinates"]] = head(coordinates(object@position), 5)
-              show[["First 5 agent sizes (pixels)"]] = head(object@NumPixels, 5)
-              # needs to print `other`
-              print(show)
-})
+# need init, show, etc methods
 
 
 
-
-### mobileAgent class extends spatialAgent by allowing movement
-setClass("mobileAgent", slots=list(heading="numeric", distance="numeric"), prototype=list(heading=NA_real_, distance=NA_real_), contains="spatialAgent")
+### mobileAgent class extends pointAgent by allowing movement
+setClass("mobileAgent", slots=list(heading="numeric", distance="numeric"),
+         prototype=list(heading=NA_real_, distance=NA_real_), contains="pointAgent")
 
 # define methods that extend already-prototyped functions in R
 setMethod("initialize", "mobileAgent", function(.Object, agentlocation = NULL, numagents=NULL, probinit=NULL, ...) {
@@ -281,7 +267,7 @@ setMethod("initialize", "mobileAgent", function(.Object, agentlocation = NULL, n
 #    nas = is.na(heading1)
 #    if (sum(nas)>0) heading1[nas] = runif(sum(nas),0,360)
     .Object@ID = as.character(1:numagents)
-    .Object@position = position
+    .object@spatial = position
     .Object@heading = heading1
     .Object@distance = distance
     
@@ -292,8 +278,8 @@ setMethod("show",
     signature = "mobileAgent",
     definition = function(object) {
         show = list()
-        show[["N"]] = paste("There are",length(object@position),"agents")
-        show[["First 5 agent coordinates"]] = head(coordinates(object@position), 5)
+        show[["N"]] = paste("There are",length(object@spatial),"agents")
+        show[["First 5 agent coordinates"]] = head(coordinates(object@spatial), 5)
         show[["First 5 agent ids"]] = head(object@ID, 5)
         print(show)
 })
@@ -308,154 +294,13 @@ setMethod("head",
 setMethod("points",
           signature = "mobileAgent",
           definition = function(x, which.to.plot=NULL, ...) {
-            # identical to definition in `spatialAgent`
+            # identical to definition in `pointAgent`
             #  should be inherited from that class already
               if (is.null(which.to.plot)) { sam = 1:length(x)} else {sam = which.to.plot}
               points(x@position@coords[sam,], ...)
 })
 
-
-setClass("rasterList", slots=c(maps="list"))
-
-setMethod("plot",
-          signature = "rasterList",
-          definition = function(x, y, which.to.plot = "all", speedup = 100, axes = "L", ...) {
-            map.list = x
-            if (length(which.to.plot)==1) {if(which.to.plot=="all") wh = 1:length(map.list) else wh = which.to.plot
-            } else {
-              wh = which.to.plot
-            }
-            
-            ext = extent(map.list[[1]])
-            if(dev.cur()==1) {
-              dev.new(height=8, width=10)
-            }
-            
-            ds = dev.size()
-            ds.ratio = ds[1]/ds[2]
-            
-            col.by.row = data.frame(matrix(ncol = 2, nrow = length(wh)))
-            
-            col.by.row[,1] = ceiling(length(wh)/(1:length(wh)))
-            col.by.row[,2] = ceiling(length(wh)/col.by.row[,1])
-            
-            wh.best = which.min(abs(apply(col.by.row,1,function(x) x[1]/x[2]) - ds.ratio))
-            
-            cols = col.by.row[wh.best,1]
-            rows = col.by.row[wh.best,2]
-            
-            row.col = min(rows/cols*ds[1]/ds[2] , 1)
-            col.row = min(cols/rows*ds[2]/ds[1] , 1)
-            vp = list()
-            
-            grid.newpage()
-            if (axes != "none" & axes != FALSE) {
-              atx = seq(xmin(ext),xmax(ext),length.out=5)
-              aty = seq(ymin(ext),ymax(ext),length.out=5)
-            }
-            cr = expand.grid(cols=((1:cols/cols - 1/cols/2)-0.55)*0.9+0.55,rows=((1:rows/rows - 1/rows/2)-0.55)*0.9+0.55)
-            
-            for (w in wh) {
-              i = match(w,wh)
-              
-              
-              vp[[i]] <- viewport(x = cr[i,"cols"], y = cr[i,"rows"], w = 1/cols*0.8, h = 1/rows*0.8,
-                                  just = c(0.5, 0.5),
-                                  name = names(map.list)[w],
-                                  xscale = c(xmin(ext),xmax(ext))/col.row,yscale= c(ymin(ext),ymax(ext))/row.col)
-              pushViewport(vp[[i]])
-              grid.raster(as.raster(map.list[[w]],maxpixels=1e4/(cols*rows)*prod(ds)/speedup))
-              if (axes != "none" & axes != FALSE) {
-                if (axes == "L") {
-                  if (cr$cols[i]==min(cr$cols)) {
-                    grid.yaxis(gp=gpar(cex=0.5),at = aty, label = aty)
-                  }
-                  if (cr$rows[i] == min(cr$rows)) {
-                    grid.xaxis(gp=gpar(cex=0.5),at = atx, label = atx)
-                  }
-                } else {
-                  grid.xaxis(gp=gpar(cex=0.5),at = atx, label = atx)
-                  grid.yaxis(gp=gpar(cex=0.5),at = aty, label = aty)
-                }
-              }
-              grid.text(names(map.list)[w], y = 1.05, vjust = 0.5, gp = gpar(cex=1-0.025*length(wh)))
-              upViewport()
-            }
-            })
-
-
-
 # define our custom methods, which need to be prototyped
-
-# This creates a rasterList class, very general, no checking... and plot function
-setClass("rasterList", slots=c(maps="list"))
-
-setMethod("plot",
-          signature = "rasterList",
-          definition = function(x, y, which.to.plot = "all", speedup = 100, axes = "L", ...) {
-            map.list = x
-            if (length(which.to.plot)==1) {if(which.to.plot=="all") wh = 1:length(map.list) else wh = which.to.plot
-            } else {
-              wh = which.to.plot
-            }
-            
-            ext = extent(map.list[[1]])
-            if(dev.cur()==1) {
-              dev.new(height=8, width=10)
-            }
-            
-            ds = dev.size()
-            ds.ratio = ds[1]/ds[2]
-            
-            col.by.row = data.frame(matrix(ncol = 2, nrow = length(wh)))
-            
-            col.by.row[,1] = ceiling(length(wh)/(1:length(wh)))
-            col.by.row[,2] = ceiling(length(wh)/col.by.row[,1])
-            
-            wh.best = which.min(abs(apply(col.by.row,1,function(x) x[1]/x[2]) - ds.ratio))
-            
-            cols = col.by.row[wh.best,1]
-            rows = col.by.row[wh.best,2]
-            
-            row.col = min(rows/cols*ds[1]/ds[2] , 1)
-            col.row = min(cols/rows*ds[2]/ds[1] , 1)
-            vp = list()
-            
-            grid.newpage()
-            if (axes != "none" & axes != FALSE) {
-              atx = seq(xmin(ext),xmax(ext),length.out=5)
-              aty = seq(ymin(ext),ymax(ext),length.out=5)
-            }
-            cr = expand.grid(cols=((1:cols/cols - 1/cols/2)-0.55)*0.9+0.55,rows=((1:rows/rows - 1/rows/2)-0.55)*0.9+0.55)
-            
-            for (w in wh) {
-              i = match(w,wh)
-              
-              
-              vp[[i]] <- viewport(x = cr[i,"cols"], y = cr[i,"rows"], w = 1/cols*0.8, h = 1/rows*0.8,
-                                  just = c(0.5, 0.5),
-                                  name = names(map.list)[w],
-                                  xscale = c(xmin(ext),xmax(ext))/col.row,yscale= c(ymin(ext),ymax(ext))/row.col)
-              pushViewport(vp[[i]])
-              grid.raster(as.raster(map.list[[w]],maxpixels=1e4/(cols*rows)*prod(ds)/speedup))
-              if (axes != "none" & axes != FALSE) {
-                if (axes == "L") {
-                  if (cr$cols[i]==min(cr$cols)) {
-                    grid.yaxis(gp=gpar(cex=0.5),at = aty, label = aty)
-                  }
-                  if (cr$rows[i] == min(cr$rows)) {
-                    grid.xaxis(gp=gpar(cex=0.5),at = atx, label = atx)
-                  }
-                } else {
-                  grid.xaxis(gp=gpar(cex=0.5),at = atx, label = atx)
-                  grid.yaxis(gp=gpar(cex=0.5),at = aty, label = aty)
-                }
-              }
-              grid.text(names(map.list)[w], y = 1.05, vjust = 0.5, gp = gpar(cex=1-0.025*length(wh)))
-              upViewport()
-            }
-          })
-
 
 setGeneric("arrow", function(agent, ...) {
     standardGeneric("arrow")
@@ -468,8 +313,6 @@ setMethod("arrow",
               co.lpos = calculate.last.position() #coordinates(agent@last.pos)
               arrows(co.lpos[,"x"], co.lpos[,"y"], co.pos[,"x"], co.pos[,"y"], length = length, ...)
 })
-
-#setGeneric("mobileAgent", function(object) standardGeneric("mobileAgent")) # remove?
 
 
 
