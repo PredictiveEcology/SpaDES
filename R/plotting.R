e296ecf47a92afaef3a5317ec0c18ba3bbb5c1ff
eliotmcintire <eliotmcintire@gmail.com>
2014-03-28 23:32:08 -0700

    convert plot to simplot
    Now it is a generic function that can take any class. So far, only
    RasterStack class is done.

--- R/plotting.R
+++ R/plotting.R
@@ -1,59 +1,58 @@
-# This creates a rasterList class, very general, no checking... and plot function
-setClass("rasterList", slots=c(maps="list"))
+##############################################################
+#' Ploting methods
+#'
+#' Plotting for RasterStack, Agents and other classes used in simulations.
+#' 
+#' For class \code{RasterStack}, this method plots many rasters. This method automatically plots individual 
+#' rasters in with an arrangement on the plotting window that is optimal for the size and shape of 
+#' the window. \code{Speedup} will make downsample the number of pixels, allowing for greater plotting
+#' speed, at a cost of more fuzzy plots. 
+#'
+#' @param x rasterStack object.
+#'
+#' @return Creates a plot within the active plotting device.
+#' 
+#' #@seealso \code{\link{grid.raster}}
+#' 
+#' @import grid
+#' @import raster
+#' @export
+#' @docType methods
+#' @rdname simplot
+#'
+# @examples
+# needs examples
+setGeneric("simplot", function(x,...) {
+           standardGeneric("simplot")
+})
 
 # which functions are used from what packages
 # needs grid package for 10 functions
 
-##############################################################
-#' Plot \code{rasterList} Class
-#'
-#' Extends \code{plot} function for use in simulations.
-#' 
-#' This method is designed to plot many rasters that are organized into a \code{rasterList}, which 
-#' is simply a list of rasters with a specified class. This method automatically plots individual 
-#' rasters in with an arrangement on the plotting window that is optimal for the size and shape of 
-#' the window. \code{Speedup} will make downsample the number of pixels, allowing for greater plotting
-#' speed, at a cost of more fuzzy plots.
-#'
-#' @param x rasterList object.
-#'
-#' @param which.to.plot numeric or character vector identifying which rasters in \code{rasterList} to plot.
+#' @param which.to.plot numeric or character vector identifying which rasters in \code{rasterStack} to plot.
 #' 
 #' @param speedup scalar indicating how much faster than normal to make plots (see Details).
 #' 
 #' @param axes string either "all", "L", or "none" (see Details). Default is "L".
 #' 
 #' @param add logical indicating whether to plot new maps (F) or update exising maps (T). Default is F.
-#'
-#' @return Creates a plot within the active plotting device.
-#' 
-#' #@seealso \code{\link{plot}} and \code{\link{rasterList}}
-#' 
-#' @import grid
-#' @import raster
-#' @export
-#' @docType methods
-#' @rdname plot
-#'
-# @examples
-# needs examples
-setMethod("plot",
-          signature = "rasterList",
-          definition = function(x, ..., which.to.plot = "all", speedup = 100, axes = "L", add = F) {
-              map.list = x
-              nam = names(map.list)
+#' @rdname simplot
+setMethod("simplot",
+          signature = "RasterStack",
+          definition = function(x, which.to.plot = "all", speedup = 100, axes = "L", add = F,... ) {
+              nam = names(x)
               
               if (length(which.to.plot)==1) {
                   if(which.to.plot=="all")
-                      wh = 1:length(map.list)
+                      wh = 1:dim(x)[3]
                   else
                       wh = which.to.plot
               } else {
                   wh = which.to.plot
               }
-              if (is.character(wh)) if (any(is.na(match(wh, names(map.list))))) stop("Not a named map in rasterList")
+              if (is.character(wh)) if (any(is.na(match(wh, names(x))))) stop("Not a named map in rasterx")
               
-              ext = extent(map.list[[1]])
+              ext = extent(x)
               if(dev.cur()==1) {
                   dev.new(height=8, width=10)
               }
@@ -97,10 +96,9 @@ setMethod("plot",
                       vp[[i]] <- viewport(x = cr[i,"cols"], y = cr[i,"rows"], w = 1/cols*0.8, h = 1/rows*0.8,
                                           just = c(0.5, 0.5),
                                           name = w,
-                                          #                                    xscale = c(xmin(ext),xmax(ext))/col.row,yscale= c(ymin(ext),ymax(ext))/row.col)
                                           xscale = c(xmin(ext),xmax(ext)),yscale= c(ymin(ext),ymax(ext)))
                       pushViewport(vp[[i]])
-                      grid.raster(as.raster(map.list[[w]],maxpixels=1e4/(cols*rows)*prod(ds)/speedup))
+                      grid.raster(as.raster(x[[w]],maxpixels=1e4/(cols*rows)*prod(ds)/speedup))#,...)
                       if (axes != "none" & axes != FALSE) {
                           if (axes == "L") {
                               if (cr$cols[i]==min(cr$cols)) {
@@ -114,15 +112,15 @@ setMethod("plot",
                               grid.yaxis(gp=gpar(cex=0.5),at = ats[["y"]], label = ats[["y"]])
                           }
                       }
-                      grid.text(names(map.list)[ma], y = 1.05, vjust = 0.5, gp = gpar(cex=1-0.015*length(wh)))
+                      grid.text(names(x)[ma], y = 1.05, vjust = 0.5, gp = gpar(cex=1-0.015*length(wh)))
                       upViewport()
                   }
               } else if (add == T){
                   for (i in wh) {
                       vp.names = sapply(current.vpTree()$children, function(x) x$name)
-                      if (is.numeric(i)) i = vp.names[i]
+                      if (is.numeric(i)) i = nam[i]#match(nam,vp.names)
                       seekViewport(i)
-                      grid.raster(as.raster(map.list[[i]],maxpixels=1e4/(length(vp.names))*prod(ds)/speedup))
+                      grid.raster(as.raster(x[[i]],maxpixels=1e4/(length(vp.names))*prod(ds)/speedup))
                       upViewport()
                   }
               }
