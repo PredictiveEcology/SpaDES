0713b22109c6d5bcfc4db3707bad282a0650abb6
SteveCumming <stevec.boreal@gmail.com>
2014-04-12 12:47:57 -0700

    Added stubs for polymorphous SpreadProb and Persistance (a param that controls how many steps a burning cell stays burny)

--- R/SpreadProcess.R
+++ R/SpreadProcess.R
@@ -1,12 +1,21 @@
-#' @param Raster* Landscape
+#' @param  Landscape
+#' @param Loci
 #' @param Mask  non null, a raster congruent with Landscape whose elements are 0,1 where 1 indicates can not spread to.
 #  DOCUMENT ME
 # tst<-raster(nrows=10,ncols=10,vals=0)
 #' 
 SpreadEvents<-
-    function(Landscape, Loci=NULL, Mask=NULL,
-             directions=8, SpreadProb=0.1,MaxSize=NULL){
+    function(Landscape, Loci=NULL,SpreadProb=0.1,
+             Persistance=NULL,Mask=NULL,MaxSize=NULL,directions=8){
    # should sanity check map extents
+        
+    is.prob<-function(x){
+        if (!is.numeric(x)) 
+            return(FALSE)
+        else 
+            return(!(x>1|| x<0))
+    }
+    
     if (is.null(Loci))
         Loci <- (Landscape@nrows/2 + 0.5) * Landscape@ncols    
 
@@ -28,14 +37,27 @@ SpreadEvents<-
      
       #select which potentials actually happened
       #nrow() only works if Potentials is an array
-      ItHappened<-runif(nrow(Potentials))<SpreadProb
+      if (is.numeric(SpreadProb))
+        ItHappened<-runif(nrow(Potentials))<SpreadProb
+      else
+        stop("Unsupported type:SpreadProb") #methods for raster* or function args
       Events<-Potentials[ItHappened,2]
       #print(Events)
       #update eligibility map
       Spreads[Events]<-n
       n<-n+1
       #drop or keep Loci
-      Loci<-Loci[runif(length(Loci)<0.05)]
+      
+      if (is.null(Persistance))
+          Loci<-NULL
+      else {
+          if (is.prob(Persistance)) 
+            Loci<-Loci[runif(length(Loci))<Persistance]
+          else
+            stop("Unsupported type: Persistance") #here is were we would handle methods
+                                                  #for raster* or functions
+      }
+     
       Loci<-c(Loci,Events)
       
     }
