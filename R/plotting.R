1899217cdc3fbdd6c6a0670229aee66cd7ad27c5
Alex Chubaty <alex.chubaty@gmail.com>
2014-04-30 11:25:10 -0700

    added `plot2dev()`
    a cross-platform implementation of Eliot's `wind()`
    - renamed it for clarity

--- R/plotting.R
+++ R/plotting.R
@@ -23,29 +23,53 @@
 #' Linux: open device with \code{x11()}.
 #' Windows: open device with \code{windows()}.
 #' 
-#' @return Opens a new plot device on the screen.
+#' @param ... Additional arguments passed to quartz(), x11(), or windows().
 #' 
-#' @param ... Additional arguments passed to x11() or windows() 
+#' @return Opens a new plot device on the screen.
 #' 
 #' @export
 #' @docType methods
-#' @rdname newPlot
+#' @rdname plot2
 #'
 # @examples
 # needs examples
 newPlot = function(...) {
-    if (Sys.info()[["sysname"]]=="Darwin") {
-        quartz(...)
-    } else if (Sys.info()[["sysname"]]=="Linux") {
-        x11(...)
-    } else if (Sys.info()[["sysname"]]=="Windows") {
-        windows(...)
-    } else {
-        x11(...) # try x11() to see if it works
-        print("Which operating system are you using?")
-    }
+  if (Sys.info()[["sysname"]]=="Darwin") {
+    quartz(...)
+  } else if (Sys.info()[["sysname"]]=="Linux") {
+    x11(...)
+  } else if (Sys.info()[["sysname"]]=="Windows") {
+    windows(...)
+  } else {
+    x11(...) # try x11() to see if it works
+    print("Which operating system are you using?")
+  }
 }
 
+##############################################################
+#' Specify where to plot
+#'
+#' Switch to an existing plot device, or if not already open,
+#' launch a new graphics device based on operating system used.
+#' 
+#' For example, \code{plot2(6)} switches tho active plot device to device #6.
+#' If it doesn't exist, it opens it. NOTE: if devices 1-5 don't exist
+#' they will be opened too.
+#' 
+#' @param ... Additional arguments passed to quartz(), x11(), or windows().
+#' 
+#' @return Opens a new plot device on the screen.
+#' 
+#' @export
+#' @docType methods
+#' @rdname plot2-method
+#'
+# @examples
+# needs examples
+plot2dev = function(x, ...) {
+  if(is.null(dev.list())) newPlot(...)
+  while (dev.set(x)!=x) newPlot(...)
+}
 
 ##############################################################
 #' Plotting methods
