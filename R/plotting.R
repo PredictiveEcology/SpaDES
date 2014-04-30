8723bba52dacaa94261317176b421da6eab2caaa
Alex Chubaty <alex.chubaty@gmail.com>
2014-04-30 12:09:48 -0700

    updated plot2dev rdnames

--- R/plotting.R
+++ R/plotting.R
@@ -13,39 +13,6 @@
 #DONE 2. use arrange.simplot for pointAgent
 # 3. allow plotting of legends
 
-
-
-##############################################################
-#' Open a new plotting window
-#'
-#' Launch a new graphics device based on operating system used.
-#' Mac OS: open device with \code{quartz()}.
-#' Linux: open device with \code{x11()}.
-#' Windows: open device with \code{windows()}.
-#' 
-#' @param ... Additional arguments passed to quartz(), x11(), or windows().
-#' 
-#' @return Opens a new plot device on the screen.
-#' 
-#' @export
-#' @docType methods
-#' @rdname plot2
-#'
-# @examples
-# needs examples
-newPlot = function(...) {
-  if (Sys.info()[["sysname"]]=="Darwin") {
-    quartz(...)
-  } else if (Sys.info()[["sysname"]]=="Linux") {
-    x11(...)
-  } else if (Sys.info()[["sysname"]]=="Windows") {
-    windows(...)
-  } else {
-    x11(...) # try x11() to see if it works
-    print("Which operating system are you using?")
-  }
-}
-
 ##############################################################
 #' Specify where to plot
 #'
@@ -62,13 +29,44 @@ newPlot = function(...) {
 #' 
 #' @export
 #' @docType methods
-#' @rdname plot2-method
+#' @rdname plot2dev-method
 #'
 # @examples
 # needs examples
 plot2dev = function(x, ...) {
   if(is.null(dev.list())) newPlot(...)
   while (dev.set(x)!=x) newPlot(...)
+}
+
+##############################################################
+#' Open a new plotting window
+#'
+#' Launch a new graphics device based on operating system used.
+#' Mac OS: open device with \code{quartz()}.
+#' Linux: open device with \code{x11()}.
+#' Windows: open device with \code{windows()}.
+#' 
+#' @param ... Additional arguments passed to quartz(), x11(), or windows().
+#' 
+#' @return Opens a new plot device on the screen.
+#' 
+#' @export
+#' @docType methods
+#' @rdname plot2dev-method
+#'
+# @examples
+# needs examples
+newPlot = function(...) {
+  if (Sys.info()[["sysname"]]=="Darwin") {
+    quartz(...)
+  } else if (Sys.info()[["sysname"]]=="Linux") {
+    x11(...)
+  } else if (Sys.info()[["sysname"]]=="Windows") {
+    windows(...)
+  } else {
+    x11(...) # try x11() to see if it works
+    print("Which operating system are you using?")
+  }
 }
 
 ##############################################################
