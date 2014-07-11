05285242ed06f513aa9211d239713016bc4febed
eliotmcintire <eliotmcintire@gmail.com>
2014-07-10 23:52:18 -0700

    even faster adj 10x !
    Now, gives option to keep data.table or return a matrix as adjacent
    function does.

--- R/better-adjacent.R
+++ R/better-adjacent.R
@@ -1,11 +1,24 @@
 ##############################################################
 #' Fast Adjacent function
 #'
-#' Faster function for determining the cells of the 4 or 8 neighbours of the \code{cells}
+#' Faster function for determining the cells of the 4, 8 or bishop
+#'  neighbours of the \code{cells}
 #' 
-#' There is some speed gain if NumCol and NumCells are passed rather than a raster. 
-#' This gain is on the order of 5%-10%. The fastest gain over \code{\link{adjacent}}
-#' is from the use of data.table pass by reference feature.
+#' About 10x speed gains over \code{adjacent} in raster package. There is some extra 
+#' speed gain if NumCol and NumCells are passed rather than a raster. 
+#' Efficiency gains come from 
+#'  1. use data.table internally
+#'   - no need to remove NAs because wrapped or outside points are
+#'     just removed directly with data.table
+#'   - use data.table to sort and fast select (though not fastest possible)   
+#'  2. don't make intermediate objects; just put calculation into return statement
+#'  
+#' The steps used in the algorithm are:
+#' 1. Calculate indices of neighbouring cells
+#' 2. Remove "to" cells that are 
+#'    - <1 or >numCells (i.e., they are above or below raster)
+#'    - where the modulo of "to" cells is equal to 1 if "from" cells are 0 (wrapped right to left)
+#'    - or where the modulo of the "to" cells is equal to 0 if "from" cells are 1 (wrapped left to right)
 #' 
 #' @param x Raster* object for which adjacency will be calculated.
 #' 
@@ -15,9 +28,14 @@
 #' 
 #' @param pairs logical. If TRUE, a matrix of pairs of adjacent cells is returned. If FALSE, a vector of cells adjacent to cells is returned
 #' 
+#' @param include logical. Should the focal cells be included in the result?
+#' 
 #' @param numCol numeric indicating number of columns in the raster. Using this with numCell is a bit faster execution time.
 #' 
 #' @param numCell numeric indicating number of cells in the raster. Using this with numCol is a bit faster execution time.
+#' 
+#' @param as.data.table logical. Should return be a data.table (or matrix to replicate \code{adjacent}). 
+#' Default is TRUE, which is faster, and has also id (as \code{adjacent})
 #' 
 #' @return a matrix of one or two columns, from and to.
 #' 
@@ -36,7 +54,8 @@
 #' numCell <- ncell(a)
 #' adj.new <- adj(numCol=numCol,numCell=numCell,sam,directions=8)
 #' print(head(adj.new))
-adj <- function(x=NULL,cells,directions=8,pairs=TRUE,numCol=NULL,numCell=NULL) {
+adj <- function(x=NULL,cells,directions=8,pairs=TRUE,include=FALSE,
+                numCol=NULL,numCell=NULL,as.data.table=TRUE) {
     if (is.null(numCol) | is.null(numCell)) {
         if (is.null(x)) stop("must provide either numCol & numCell or a x")
         numCol = ncol(x)
@@ -53,43 +72,43 @@ adj <- function(x=NULL,cells,directions=8,pairs=TRUE,numCol=NULL,numCell=NULL) {
         botl=as.integer(cells+numCol-1)
         bot=as.integer(cells+numCol)
         botr=as.integer(cells+numCol+1)
-        adj=data.table(from=rep.int(cells,times=directions),to=c(topl,top,topr,lef,rig,botl,bot,botr),key="from")
+        if (include)
+          adj=data.table(from=rep.int(cells,times=directions+include),
+                       to=c(topl,top,topr,lef,as.integer(cells),rig,botl,bot,botr),key="from")
+        else
+          adj=data.table(from=rep.int(cells,times=directions+include),
+                       to=c(topl,top,topr,lef,rig,botl,bot,botr),key="from")
     } else if (directions==4) {
         # determine the indices of the 4 surrounding cells of the cells cells
         top=as.integer(cells-numCol)
         lef=as.integer(cells-1)
         rig=as.integer(cells+1)
         bot=as.integer(cells+numCol)
-        adj=data.table(from=rep.int(cells,times=4),to=c(top,lef,rig,bot),key="from")
+        if (include)
+          adj=data.table(from=rep.int(cells,times=5),to=c(top,lef,as.integer(cells),rig,bot),key="from")
+        else
+          adj=data.table(from=rep.int(cells,times=4),to=c(top,lef,rig,bot),key="from")
     } else if (directions=="bishop") {
         topl=as.integer(cells-numCol-1)
         topr=as.integer(cells-numCol+1)
         botl=as.integer(cells+numCol-1)
         botr=as.integer(cells+numCol+1)
-        adj=data.table(from=rep.int(cells,times=directions),to=c(topl,topr,botl,botr),key="from")
+        if (include)
+          adj=data.table(from=rep.int(cells,times=5),
+                       to=c(topl,topr,as.integer(cells),botl,botr),key="from")
+        else
+          adj=data.table(from=rep.int(cells,times=4),
+                       to=c(topl,topr,botl,botr),key="from")
     } else {stop("directions must be 4 or 8 or \'bishop\'")}
     
-    # Add 2 columns that show whether the from or to cell is one of the far
-    #  right or far left cells in x
-    #adj[,`:=`(from.mod.numCol=from%%numCol,to.mod.numCol=to%%numCol)]
-    to.mod.numCol=adj$to%%numCol
-    from.mod.numCol=adj$from%%numCol
-    
-    # remove any cell that is outside extent or wrapped (above, below, right or left)
-    adj.return <- adj[
-        i = !(to<1 |  #above top row
-                  to>numCell | # below bottom row
-                  (from.mod.numCol==0 & to.mod.numCol==1) | #right edge cell,with neighbours wrapped
-                  (from.mod.numCol==1 & to.mod.numCol==0)),  #left edge cell,with neighbours wrapped
-        ]
-    
-    
-    #remove unwanted temporary columns by reference, based on pairs
-#     if (pairs) {
-#         adj.return[,`:=`(from.mod.numCol=NULL,to.mod.numCol=NULL)]
-#     } else {
-#         adj.return[,`:=`(from=NULL,from.mod.numCol=NULL,to.mod.numCol=NULL)]
-#     }
-    
-    return(as.matrix(adj.return))
+    if (as.data.table)
+      return(adj[
+        i = !((to%%numCell!=to) |  #top or bottom of raster
+                ((adj$from%%numCol+adj$to%%numCol)==1))# | #right & left edge cells,with neighbours wrapped
+        ])
+    else 
+      return(as.matrix(adj[
+        i = !((to%%numCell!=to) |  #top or bottom of raster
+                ((adj$from%%numCol+adj$to%%numCol)==1))# | #right & left edge cells,with neighbours wrapped
+        ]))
 }
--- SAMPLE/Eliot data.table.r
+++ SAMPLE/Eliot data.table.r
@@ -1,59 +1,25 @@
+devtools::load_all("c:/Eliot/GitHub/SpaDES")
 library(microbenchmark)
 library(raster)
-library(SpaDES)
+#library(SpaDES)
 a = raster(extent(0,1e3,0,1e3),res=1)
 cells = sample(1:ncell(a),1e3)
 numCol <- ncol(a)
 numCell <- ncell(a)
+directions=8
 
   
-
-(mb = microbenchmark(times=50L,
+(mb = microbenchmark(times=100L,
+                     out1 = rep.int(cells,4),
+                     out
+                     
+(mb = microbenchmark(times=100L,
   adj.orig = adjacent(a,cells,sort=T,directions=8),
   adj.new = adj(numCol=numCol,numCell=numCell,cells=cells,directions=8),
-#  adj.new1 = adj1(numCol=numCol,numCell=numCell,cells=cells,directions=8),
-#  adj.new2 = adj2(numCol=numCol,numCell=numCell,cells=cells,directions=8),
-  adj.new3 = adj3(numCol=numCol,numCell=numCell,cells=cells,directions=8)
+  adj.new3 <- adj3(numCol=numCol,numCell=numCell,cells=cells,directions=8)
 ))
-
+print(all.equal(adj.orig,adj.new3))
 ##############################################################
-#' Fast Adjacent function
-#'
-#' Faster function for determining the cells of the 4 or 8 neighbours of the \code{cells}
-#' 
-#' There is some speed gain if NumCol and NumCells are passed rather than a raster. 
-#' This gain is on the order of 5%-10%. The fastest gain over \code{\link{adjacent}}
-#' is from the use of data.table pass by reference feature.
-#' 
-#' @param x Raster* object for which adjacency will be calculated.
-#' 
-#' @param cells vector of cell numbers for which adjacent cells should be found. Cell numbers start with 1 in the upper-left corner and increase from left to right and from top to bottom
-#' 
-#' @param directions the number of directions in which cells should be connected: 4 (rook's case), 8 (queen's case), or 'bishop' to connect cells with one-cell diagonal moves. Or a neigborhood matrix (see Details) 
-#' 
-#' @param pairs logical. If TRUE, a matrix of pairs of adjacent cells is returned. If FALSE, a vector of cells adjacent to cells is returned
-#' 
-#' @param numCol numeric indicating number of columns in the raster. Using this with numCell is a bit faster execution time.
-#' 
-#' @param numCell numeric indicating number of cells in the raster. Using this with numCol is a bit faster execution time.
-#' 
-#' @return a matrix of one or two columns, from and to.
-#' 
-#' @seealso \code{\link{adjacent}}
-#' 
-#' @import data.table
-#' @export
-#' @docType methods
-#' @rdname adj
-#'
-#' @examples
-#' require(raster)
-#' a <- raster(extent(0,1000,0,1000),res=1)
-#' sam = sample(1:length(a),1e4)
-#' numCol <- ncol(a)
-#' numCell <- ncell(a)
-#' adj.new <- adj(numCol=numCol,numCell=numCell,sam,directions=8)
-#' print(head(adj.new))
 adj3 <- function(x=NULL,cells,directions=8,pairs=TRUE,numCol=NULL,numCell=NULL) {
   if (is.null(numCol) | is.null(numCell)) {
     if (is.null(x)) stop("must provide either numCol & numCell or a x")
@@ -71,7 +37,8 @@ adj3 <- function(x=NULL,cells,directions=8,pairs=TRUE,numCol=NULL,numCell=NULL) 
     botl=as.integer(cells+numCol-1)
     bot=as.integer(cells+numCol)
     botr=as.integer(cells+numCol+1)
-    adj=data.table(from=rep.int(cells,times=directions),to=c(topl,top,topr,lef,rig,botl,bot,botr),key="to")
+    adj=data.table(from=rep.int(cells,times=directions),
+                   to=c(topl,top,topr,lef,rig,botl,bot,botr),key="from")
   } else if (directions==4) {
     # determine the indices of the 4 surrounding cells of the cells cells
     top=as.integer(cells-numCol)
@@ -84,40 +51,14 @@ adj3 <- function(x=NULL,cells,directions=8,pairs=TRUE,numCol=NULL,numCell=NULL) 
     topr=as.integer(cells-numCol+1)
     botl=as.integer(cells+numCol-1)
     botr=as.integer(cells+numCol+1)
-    adj=data.table(from=rep.int(cells,times=directions),to=c(topl,topr,botl,botr),key="from")
+    adj=data.table(from=rep.int(cells,times=directions),
+                   to=c(topl,topr,botl,botr),key="from")
   } else {stop("directions must be 4 or 8 or \'bishop\'")}
-  
-  # Add 2 columns that show whether the from or to cell is one of the far
-  #  right or far left cells in x
-  adj[,`:=`(from.mod.numCol=from%%numCol,to.mod.numCol=to%%numCol)]
-  
-#  to.mod.numCol=adj$to%%numCol
-#  from.mod.numCol=adj$from%%numCol
-  
-#   first = adj$to<1
-#   second = adj$to>numCell
-#   third = (from.mod.numCol==0 & to.mod.numCol==1)
-#   fourth = (from.mod.numCol==1 & to.mod.numCol==0)
-#   dt <- data.table(first,second,third, fourth,key=c("first","second","third","fourth"))
-setkey(adj,to,from.mod.numCol,to.mod.numCol)  
-  # remove any cell that is outside extent or wrapped (above, below, right or left)
-#  adj.return <- adj[!(first | second | third | fourth)]
-  adj.return <- adj[
-    !J(unique(to),0,1)][!J(unique(to),1,0)][J(unique(to))]
-#    ]#,     #above top row
-#            numCell:(numCell+numCol)))#,  # below bottom row
-#            !CJ(unique(to),0,1)#) #right edge cell,with neighbours wrapped
-#            c(NULL,NULL,1,0))  #left edge cell,with neighbours wrapped
-    #]
-#setkey(adj,from.mod.numCol,to.mod.numCol)  
-#adj[!J(0,1)]
-  
-  #remove unwanted temporary columns by reference, based on pairs
-  #     if (pairs) {
-  #         adj.return[,`:=`(from.mod.numCol=NULL,to.mod.numCol=NULL)]
-  #     } else {
-  #         adj.return[,`:=`(from=NULL,from.mod.numCol=NULL,to.mod.numCol=NULL)]
-  #     }
-  
-  return(as.matrix(adj.return))
+
+  adj[,keep:=!((to%%numCell!=to) |  #top or bottom of raster
+                   ((adj$from%%numCol+adj$to%%numCol)==1))]
+  #setkey(adj,keep)
+  adj.return<-adj[keep==TRUE]
+  return(as.matrix(adj.return[,keep:=NULL]# | #right & left edge cells,with neighbours wrapped
+    ))
 }
