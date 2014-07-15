883a7b7f1d90b48e0f1d457f74922fac327da39e
eliotmcintire <eliotmcintire@gmail.com>
2014-07-14 17:43:53 -0700

    Large changes to spread
    New function is faster than spread; it uses as much pass by reference as
    possible, via data.table package.
    
    I have not replaced the spread function yet. We have to see if we like
    the new one.
    
    Finally, I added mask and added maxSize
    Also a bugfix done... when potentials is just one row, R turns it to a
    numeric vector and nrow doesn't work.

--- R/SpreadProcess.R
+++ R/SpreadProcess.R
@@ -88,7 +88,7 @@ setMethod("spread",
                 
                 while ( (length(loci)>0) && (iterations>=n) ) {
                     #print(paste(n, length(loci)))
-                    potentials <- adjacent(landscape, loci, directions)
+                    potentials <- adj(landscape, loci, directions)
                     
                     # drop those ineligible
                     if (!is.null(mask)){
@@ -101,12 +101,15 @@ setMethod("spread",
                                         
                     # select which potentials actually happened
                     # nrow() only works if potentials is an array
-                    if (is.numeric(spreadProb)) {
-                        ItHappened <- runif(nrow(potentials)) <= spreadProb
+                    if (is(potentials,"numeric")) {
+                      ItHappened <- runif(1) <= spreadProb
+                      events <- potentials[2][ItHappened]
+                    } else if (is.numeric(spreadProb)) {
+                      ItHappened <- runif(nrow(potentials)) <= spreadProb
+                      events <- potentials[ItHappened, 2]
                     } else {
-                        stop("Unsupported type:spreadProb") # methods for raster* or function args
+                      stop("Unsupported type:spreadProb") # methods for raster* or function args
                     }
-                    events <- potentials[ItHappened, 2]
                     #print(events)
                     
                     # update eligibility map
@@ -131,3 +134,157 @@ setMethod("spread",
                 return(spreads)
           }
 )
+
+##############################################################
+#' Simulate a spread process on a landscape.
+#'
+#' More detailed description needed here.
+#'
+#' @param landscape     A \code{RasterLayer} object.
+#' 
+#' @param loci          A vector of locations in \code{landscape}
+#' 
+#' @param spreadProb    The probability of spreading.
+#' 
+#' @param persistance   DOCUMENTATION NEEDED
+#' 
+#' @param mask          non-NULL, a \code{RasterLayer} object congruent with \code{landscape}
+#'                      whose elements are \code{0,1}, where 1 indicates "cannot spread to".
+#' 
+#' @param maxSize       The maximum number of pixels for a fire. This is currently only a single number, 
+#' not one for each spread event
+#' 
+#' @param directions    The number adjacent cells in which to look; default is 8 (Queen case).
+#' 
+#' @param iterations    Number of iterations to spread. Leaving this \code{NULL} allows the spread
+#'                      to continue until stops spreading itself (i.e., exhausts itself).
+#' 
+#' @return A \code{RasterLayer} indicating the spread of the process in the landscape.
+#' 
+#' @import raster
+#' @export
+#' @docType methods
+#'
+#' @examples
+#'  \dontrun{tmp <- raster(nrows=10, ncols=10, vals=0)}
+#'  \dontrun{plot(tmp)}
+#'  \dontrun{tmp <- spread(tmp, spreadProb=0.225)}
+#'  \dontrun{plot(tmp)}
+#'  
+#' @name spread.adj
+#' @author Steve Cumming \email{Steve.Cumming@@sbf.ulaval.ca}
+#' @rdname spread.adj-method
+#' 
+setGeneric("spread.adj", function(landscape, loci, spreadProb, persistance, 
+                              mask, maxSize, directions, iterations, ...) {
+  standardGeneric("spread.adj")
+})
+
+#' @param plot.it    If TRUE, then plot the raster at every iteraction, so one can watch the 
+#' spread event grow.
+#' @import raster
+#' @rdname spread.adj-method
+setMethod("spread.adj",
+          signature(landscape="RasterLayer",# loci="integer", 
+                    spreadProb="numeric",
+                    #persistance="numeric", 
+                    mask="RasterLayer"#, maxSize="numeric",
+                    #directions="integer", iterations="integer"
+                    ),
+          definition = function(landscape, loci, spreadProb, persistance,
+                       mask, maxSize, directions, iterations, plot.it=FALSE) {
+  ### should sanity check map extents
+  is.prob <- function(x) {
+    if (!is.numeric(x)) 
+      return(FALSE)
+    else 
+      return(!(x>1 || x<0))
+  }
+  
+  if (is.null(loci))  {
+    # start it in the centre cell
+    loci <- (landscape@nrows/2 + 0.5) * landscape@ncols
+  }
+  
+  #spreads <- setValues(raster(landscape), 0)
+  spreads <- data.table(ind=1:ncell(landscape),burned=0,key="ind")
+  if(!is.null(mask))
+    masked<-getValues(mask)==0
+  n <- 1
+  spreads[loci,burned:=n]
+  size <- length(loci)
+  #spreads[4,burned:=NA]
+  
+  if (is.null(iterations)) {
+    iterations = Inf # this is a stupid way to do this!
+  } else {
+    # do nothing
+  }
+  
+  while ( (length(loci)>0) && (iterations>=n) ) {
+    #print(paste(n, length(loci)))
+    potentials <- adj(landscape, loci, directions,as.data.table=TRUE)
+    setkey(potentials,to)
+    
+    # drop those ineligible
+    if (!is.null(mask)){
+      potentials <- spreads[masked][potentials][burned==0][,burned:=NULL]
+    } else {
+      potentials <- spreads[potentials][burned==0][,burned:=NULL]
+    }
+    
+    # select which potentials actually happened
+    # nrow() only works if potentials is an array
+    if (!is.numeric(spreadProb)) {
+      #  ItHappened <- runif(nrow(potentials)) <= spreadProb
+      #} else {
+      stop("Unsupported type:spreadProb") # methods for raster* or function args
+    }
+    
+    events <- potentials[runif(nrow(potentials))<=spreadProb,ind]
+    
+    # Implement maxSize
+    if((size+length(events)) > maxSize) {
+      keep<-length(events) - ((size+length(events)) - maxSize)
+      events<-events[sample(length(events),keep)]
+    }
+    
+    size <- size + length(unique(events))
+    
+    # update eligibility map
+    
+    n <- n+1
+    spreads[events,burned:=n]
+    
+    if(size >= maxSize) {
+      events<-NULL
+    }
+    
+    # drop or keep loci
+    if (is.null(persistance)) {
+      loci <- NULL
+    } else {
+      if (is.prob(persistance)) {
+        loci <- loci[runif(length(loci))<=persistance]
+      } else {
+        # here is were we would handle methods for raster* or functions
+        stop("Unsupported type: persistance")
+      }
+    }
+    
+    loci <- c(loci, events)
+    
+    if (plot.it){
+      top <- raster(landscape)
+      top<-setValues(top,spreads[,burned])
+      plot(top)
+    }
+    
+  }
+  #loci = sample(1:ncell(a),20)
+  spre=raster(landscape)
+  spre<-setValues(spre, spreads[,burned])
+  return(spre)
+}
+)
+
--- SAMPLE/Eliot data.table.r
+++ SAMPLE/Eliot data.table.r
@@ -1,24 +1,81 @@
+devtools::install_github("lineprof")
+devtools::install_github("pryr")
+devtools::install_github("shiny-slickgrid", "wch")
+library(lineprof)
+library(pryr)
+library(shiny)
+
 devtools::load_all("c:/Eliot/GitHub/SpaDES")
 library(microbenchmark)
 library(raster)
+library(RColorBrewer)
+library(compiler)
+enableJIT(3)
 #library(SpaDES)
-a = raster(extent(0,1e3,0,1e3),res=1)
-hab = GaussMap(extent(a),speedup=10)
-loci = b = as.integer(sample(1:ncell(a),1e2))
+a = raster(extent(0,1e2,0,1e2),res=1)
+hab = a#GaussMap(extent(a),speedup=1000)
+loci = b = as.integer(sample(1:ncell(a),10))
+mask = raster(a)
+mask = setValues(mask, 0)
+mask[1:5000] <- 1
 numCol <- ncol(a)
 numCell <- ncell(a)
 #cells = 1:numCell
   directions=8
 
-simplot(hab,speedup=1)
+cols = list(brewer.pal(8,"RdYlGn")[8:1],brewer.pal(9,"Blues"),brewer.pal(8,"Spectral"))
+newPlot()
+simplot(hab,speedup=15,col=brewer.pal(9,"Accent"))
+simplot(stack(speedup=15,fire0,fire1,hab),col=cols)
 
-(mb <- microbenchmark(times = 10L,
-fire0 = spread(hab,loci=b,0.225,0,NULL,1e6,8,1e6),
-fire1 = spread.adjacent(hab,loci=b,0.225,0,NULL,1e6,8,1e6),
-fire2 = spread.adj(hab,loci=b,0.225,0,NULL,1e6,8,1e6)
-))
-newPlot();dev.set(4)
-simplot(stack(fire1,fire2,fire0))
+mb = list()
+for (i in 1:4){
+hab = raster(extent(0,10^i,0,10^i),res=1)
+b=as.integer(sample(1:ncell(hab),1))
+mb[[i]] <- microbenchmark(times = 10L,
+fire0 <- spread(hab,loci=b,1,0,NULL,1e3,8,1e6),
+fire1 <- spread.adjacent(hab,loci=b,1,0,NULL,1e3,8,1e6),
+fire2 <- spread.adj(hab,loci=b,1,0,NULL,1e3,8,1e6),
+fire3 <- spread.adj.c(hab,loci=b,1,0,NULL,1e3,8,1e6)
+)
+}
+
+fire2 <- spread.adj(hab,loci=b,1,0,NULL,1e3,8,1e6,mask=mask)
+
+out <-sapply(mb,function(x) print(x)[[4]])
+for (i in 1:3)
+  out[,i]<-out[,i]/1000
+
+par(mai=c(1, 1, 1, 1))
+num.pixels = (10^(1:4))^2
+fns = c("original","adj","optimized.adj","recompiled.optimized.adj")
+plot(1:4,out[,4],log="y",type="l",ylim = c(0.05,25),xlab="",axes=F,ylab="Time in seconds")
+lapply(1:4,function(x) {lines(1:4,out[,x],col=x)})
+axis(2)
+axis(1,label=fns,at=1:4)
+legend("topright",inset=c(-0.1,-0.5),xpd=NA,legend=num.pixels,lty=1,col=1:4,title="num pixels")
+
+out2 = numeric()
+for(i in 1:4)
+  out2[i] <- out[1,i]/out[3,i]
+
+plot(num.pixels,out2,log="x",ylab="speedup factor")
+mtext(side=3,"Speedup between spread fn in Raster, and new spread\nas a function of num.pixels in raster")
+
+enableJIT(0)
+system.time(fire2 <- spread.adj(hab,loci=b,1,0,NULL,1e3,8,1e6))
+enableJIT(3)
+system.time(fire3 <- spread.adj.c(hab,loci=b,1,0,NULL,1e3,8,1e6))
+
+adj.c <- compiler::cmpfun(adj)
+spread.adj.c <- compiler::cmpfun(spread.adj)
+
+prof <- lineprof(spread.adj(hab,loci=b,0.225,0,NULL,1e6,8,1e2))
+shine(prof)
+
+#newPlot();
+#dev.set(4)
+simplot(stack(fire1,fire2,fire0),speedup=1)
   
 (mb = microbenchmark(times=200L,
   adj.orig = adjacent(a,cells,sort=T,directions=8),
@@ -74,6 +131,7 @@ spread.adjacent <- function(landscape, loci, spreadProb, persistance,
   spreads <- setValues(raster(landscape), 0)
   n <- 1
   spreads[loci] <- n
+  size <- length(loci)
   
   if (is.null(iterations)) {
     iterations = Inf # this is a stupid way to do this!
@@ -81,7 +139,7 @@ spread.adjacent <- function(landscape, loci, spreadProb, persistance,
     # do nothing
   }
   
-  while ( (length(loci)>0) && (iterations>=n) ) {
+  while ( (length(loci)>0) && (iterations>=n)  ) {
     #print(paste(n, length(loci)))
     potentials <- adj(landscape, loci, directions)
     
@@ -110,11 +168,21 @@ spread.adjacent <- function(landscape, loci, spreadProb, persistance,
     }
     #print(events)
     
+    if((size+length(events)) > maxSize) {
+      keep<-length(events) - ((size+length(events)) - maxSize)
+      events<-events[sample(length(events),keep)]
+      
+    }
+    size <- size + length(unique(events))
     # update eligibility map
-    spreads[events] <- n
     n <- n+1
+    spreads[events] <- n
+    if(size >= maxSize) {
+      events<-NULL
+    }
     
     # drop or keep loci
+    
     if (is.null(persistance)) {
       loci <- NULL
     } else {
@@ -149,8 +217,12 @@ spread.adj <- function(landscape, loci, spreadProb, persistance,
   
   #spreads <- setValues(raster(landscape), 0)
   spreads <- data.table(ind=1:ncell(landscape),burned=0,key="ind")
+  if(!is.null(mask))
+    masked<-getValues(mask)==0
   n <- 1
   spreads[loci,burned:=n]
+  size <- length(loci)
+  #spreads[4,burned:=NA]
   
   if (is.null(iterations)) {
     iterations = Inf # this is a stupid way to do this!
@@ -165,17 +237,10 @@ spread.adj <- function(landscape, loci, spreadProb, persistance,
     
     # drop those ineligible
     if (!is.null(mask)){
-      tmp <- extract(mask, potentials[,to])
+      potentials <- spreads[masked][potentials][burned==0][,burned:=NULL]
     } else {
-#      tmp <- extract(spreads, potentials[,to])
-      tmp <- spreads[potentials,ind,keyby=burned]
-      tmp <- na.omit(tmp[J(0)])
-      setkey(tmp,ind)
-      #tmp<-na.omit(tmp)
-      
+      potentials <- spreads[potentials][burned==0][,burned:=NULL]
     }
-    # Can't get this to do a binary Join, but only where burned==0, i.e., not NA or not >1 
-    potentials<-potentials[tmp]#[J(unique(ind),0)],]#), FALSE, tmp[,burned]==0),]
     
     # select which potentials actually happened
     # nrow() only works if potentials is an array
@@ -184,13 +249,26 @@ spread.adj <- function(landscape, loci, spreadProb, persistance,
     #} else {
       stop("Unsupported type:spreadProb") # methods for raster* or function args
     }
-    events <- potentials[runif(nrow(potentials)) <= spreadProb, list(to)]
-    #print(events)
-    
+
+    events <- potentials[runif(nrow(potentials))<=spreadProb,ind]
+
+    # Implement maxSize
+    if((size+length(events)) > maxSize) {
+      keep<-length(events) - ((size+length(events)) - maxSize)
+      events<-events[sample(length(events),keep)]
+    }
+
+    size <- size + length(unique(events))
+
     # update eligibility map
+
     n <- n+1
     spreads[events,burned:=n]
-    
+        
+    if(size >= maxSize) {
+      events<-NULL
+    }
+
     # drop or keep loci
     if (is.null(persistance)) {
       loci <- NULL
@@ -203,7 +281,7 @@ spread.adj <- function(landscape, loci, spreadProb, persistance,
       }
     }
     
-    loci <- c(loci, events[,to])
+    loci <- c(loci, events)
 
     if (plot.it){
       top <- raster(landscape)
@@ -236,6 +314,7 @@ spread <- function(landscape, loci, spreadProb, persistance,
   spreads <- setValues(raster(landscape), 0)
   n <- 1
   spreads[loci] <- n
+  size <- length(loci)
   
   if (is.null(iterations)) {
     iterations = Inf # this is a stupid way to do this!
@@ -270,10 +349,22 @@ spread <- function(landscape, loci, spreadProb, persistance,
     #print(events)
     
     # update eligibility map
+    if((size+length(events)) > maxSize) {
+      keep<-length(events) - ((size+length(events)) - maxSize)
+      events<-events[sample(length(events),keep)]
+    }
+    size <- size + length(unique(events))
+    # update eligibility map
+
     spreads[events] <- n
     n <- n+1
     
+    if(size >= maxSize) {
+      events<-NULL
+    }
+    
     # drop or keep loci
+    
     if (is.null(persistance)) {
       loci <- NULL
     } else {
@@ -289,4 +380,99 @@ spread <- function(landscape, loci, spreadProb, persistance,
     
   }
   return(spreads)
+}
+
+spread.adj.c <- function(landscape, loci, spreadProb, persistance,
+                       mask, maxSize, directions, iterations, plot.it=FALSE) {
+  ### should sanity check map extents
+  is.prob <- function(x) {
+    if (!is.numeric(x)) 
+      return(FALSE)
+    else 
+      return(!(x>1 || x<0))
+  }
+  
+  if (is.null(loci))  {
+    # start it in the centre cell
+    loci <- (landscape@nrows/2 + 0.5) * landscape@ncols
+  }
+  
+  #spreads <- setValues(raster(landscape), 0)
+  spreads <- data.table(ind=1:ncell(landscape),burned=0,key="ind")
+  n <- 1
+  spreads[loci,burned:=n]
+  size <- length(loci)
+  #spreads[4,burned:=NA]
+  
+  if (is.null(iterations)) {
+    iterations = Inf # this is a stupid way to do this!
+  } else {
+    # do nothing
+  }
+  
+  while ( (length(loci)>0) && (iterations>=n) ) {
+    #print(paste(n, length(loci)))
+    potentials <- adj.c(landscape, loci, directions,as.data.table=TRUE)
+    setkey(potentials,to)
+    
+    # drop those ineligible
+    if (!is.null(mask)){
+      tmp <- extract(mask, potentials[,to])
+    } else {
+      #      tmp <- extract(spreads, potentials[,to])
+      potentials <- spreads[potentials][burned==0][,burned:=NULL]#,ind,keyby=burned]
+      #tmp <- na.omit(tmp[burned==0])
+      #setkey(tmp,ind)
+      #tmp<-na.omit(tmp)
+      
+    }
+    
+    # select which potentials actually happened
+    # nrow() only works if potentials is an array
+    if (!is.numeric(spreadProb)) {
+      #  ItHappened <- runif(nrow(potentials)) <= spreadProb
+      #} else {
+      stop("Unsupported type:spreadProb") # methods for raster* or function args
+    }
+    
+    events <- potentials[runif(nrow(potentials))<=spreadProb,ind]
+    
+    if((size+length(events)) > maxSize) {
+      keep<-length(events) - ((size+length(events)) - maxSize)
+      events<-events[sample(length(events),keep)]
+    }
+    size <- size + length(unique(events))
+    # update eligibility map
+    n <- n+1
+    spreads[events,burned:=n]
+    
+    if(size >= maxSize) {
+      events<-NULL
+    }
+    # drop or keep loci
+    
+    if (is.null(persistance)) {
+      loci <- NULL
+    } else {
+      if (is.prob(persistance)) {
+        loci <- loci[runif(length(loci))<=persistance]
+      } else {
+        # here is were we would handle methods for raster* or functions
+        stop("Unsupported type: persistance")
+      }
+    }
+    
+    loci <- c(loci, events)
+    
+    if (plot.it){
+      top <- raster(landscape)
+      top<-setValues(top,spreads[,burned])
+      plot(top)
+    }
+    
+  }
+  #loci = sample(1:ncell(a),20)
+  spre=raster(landscape)
+  spre<-setValues(spre, spreads[,burned])
+  return(spre)
 }
