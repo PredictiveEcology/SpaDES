85c2ad2100031cbe0658b608ba72f288df800f42
eliotmcintire <eliotmcintire@gmail.com>
2014-07-16 23:04:10 -0700

    comment out initialize function for mobile agent

--- R/initialize.R
+++ R/initialize.R
@@ -115,98 +115,98 @@ spec.num.per.patch = function(patches, num.per.patch.table=NULL, num.per.patch.m
 
 
 ###
-### INCORPORATE RELEVANT PARTS OF THIS OLD INIT FUNCTION INTO INITCOODRS()
-###
-#' initialize mobileAgent
-#' 
-#' @param agentlocation The initial positions of the agents
-#'                      (currently only \code{RasterLayer} or
-#'                      \code{SpatialPolygonsDataFrame}) accepted.
-#' 
-#' @param numagents The number of agents to initialize.
-#' 
-#' @param probinit The probability of placing an agent at a given initial position.
-#' 
-#' @export
-setMethod("initialize", "mobileAgent", function(.Object, ..., agentlocation=NULL, numagents=NULL, probinit=NULL) {
-  if (is(agentlocation, "Raster")){
-    ext = extent(agentlocation)
-    if (!is.null(probinit)) {
-      #            nonNAs = !is.na(getvalue(probinit))
-      nonNAs = !is.na(getValues(probinit))
-      wh.nonNAs = which(nonNAs)
-      #            ProbInit.v = cumsum(getvalue(probinit)[nonNAs])
-      ProbInit.v = cumsum(getValues(probinit)[nonNAs])
-      if (!is.null(numagents)) {
-        ran = runif(numagents,0,1)
-        fI = findInterval(ran, ProbInit.v)+1
-        fI2 = wh.nonNAs[fI]
-        last.ran = runif(numagents,0,1)
-        last.fI = findInterval(last.ran, ProbInit.v)+1
-        last.fI2 = wh.nonNAs[last.fI]
-      } else {
-        #                va = getvalue(probinit)[nonNAs]
-        va = getValues(probinit)[nonNAs]
-        ran = runif(length(va), 0, 1)
-        fI2 = wh.nonNAs[ran<va]
-        
-        last.ran = runif(length(fI2), 0, 1)
-        last.fI = findInterval(last.ran, ProbInit.v) + 1
-        last.fI2 = wh.nonNAs[last.fI]
-        
-        #                last.ran = runif(length(fI2),0,1)
-        #                last.fI2 = wh.nonNAs[last.ran<va]
-      }
-      if (length(grep(pattern="Raster",class(agentlocation)))==1) {
-        position = xyFromCell(agentlocation,fI2,spatial = T)
-      } else if (length(grep(pattern="SpatialPoints",class(agentlocation)))==1) {
-        position = coordinates(agentlocation)
-      } else {
-        stop("need raster layer or Spatial Points object")
-      }
-      numagents = length(position)
-    } else {
-      # probinit is NULL - start exactly the number of agents as there
-      # are pixels in agentlocation
-      if (!is.null(numagents)) {
-        if (is(agentlocation,"Raster")) {
-          xy=matrix(runif(numagents*2,c(xmin(ext),ymin(ext)),c(xmax(ext),ymax(ext))),ncol=2,byrow=T)
-          colnames(xy)=c("x","y")
-          position = SpatialPoints(xy)
-          #                    position = SpatialPoints(sampleRandom(agentlocation, numagents, xy = T, sp = T))
-        } else if (is(agentlocation,"SpatialPoints")) {
-          sam = sample(1:length(agentlocation),numagents)
-          position = SpatialPoints(agentlocation[sam,])
-        } else {
-          stop("need raster layer or Spatial Points object")
-        }
-      } else { # for numagents also NULL
-        if (length(grep(pattern="Raster",class(agentlocation)))==1) {
-          position = SpatialPoints(xyFromCell(agentlocation,Which(agentlocation,cells=T)))
-        } else if (length(grep(pattern="SpatialPoints",class(agentlocation)))==1) {
-          position = SpatialPoints(agentlocation)
-        } else {
-          stop("need raster layer or Spatial Points object")
-        }
-        numagents = length(position)
-      }
-    }
-  } else if (is(agentlocation,"SpatialPolygonsDataFrame")) {
-    if (!is.null(numagents)) {
-      if (!is.null(pri) ) {
-        position = SpatialPoints(dotsInPolys(agentlocation,as.integer(round(numagents*pri,0))))
-        numagents = length(position)
-      } else {stop("with SpatialPolygonsDataFrame, probinit is required")}
-    } else {stop("with SpatialPolygonsDataFrame, numagents is required")}
-  } else if (is.null(agentlocation)) { stop("Need to provide agentlocation, which can be a map layer")
-  }
-  heading1 = runif(numagents, 0, 360)
-  distance = runif(numagents, 0.1, 10)
-  
-  .Object@ID = as.character(1:numagents)
-  .Object@spatial = position
-  .Object@heading = heading1
-  .Object@distance = distance
-  
-  return(.Object)
-})
+# ### INCORPORATE RELEVANT PARTS OF THIS OLD INIT FUNCTION INTO INITCOODRS()
+# ###
+# #' initialize mobileAgent
+# #' 
+# #' @param agentlocation The initial positions of the agents
+# #'                      (currently only \code{RasterLayer} or
+# #'                      \code{SpatialPolygonsDataFrame}) accepted.
+# #' 
+# #' @param numagents The number of agents to initialize.
+# #' 
+# #' @param probinit The probability of placing an agent at a given initial position.
+# #' 
+# #' @export
+# setMethod("initialize", "mobileAgent", function(.Object, ..., agentlocation=NULL, numagents=NULL, probinit=NULL) {
+#   if (is(agentlocation, "Raster")){
+#     ext = extent(agentlocation)
+#     if (!is.null(probinit)) {
+#       #            nonNAs = !is.na(getvalue(probinit))
+#       nonNAs = !is.na(getValues(probinit))
+#       wh.nonNAs = which(nonNAs)
+#       #            ProbInit.v = cumsum(getvalue(probinit)[nonNAs])
+#       ProbInit.v = cumsum(getValues(probinit)[nonNAs])
+#       if (!is.null(numagents)) {
+#         ran = runif(numagents,0,1)
+#         fI = findInterval(ran, ProbInit.v)+1
+#         fI2 = wh.nonNAs[fI]
+#         last.ran = runif(numagents,0,1)
+#         last.fI = findInterval(last.ran, ProbInit.v)+1
+#         last.fI2 = wh.nonNAs[last.fI]
+#       } else {
+#         #                va = getvalue(probinit)[nonNAs]
+#         va = getValues(probinit)[nonNAs]
+#         ran = runif(length(va), 0, 1)
+#         fI2 = wh.nonNAs[ran<va]
+#         
+#         last.ran = runif(length(fI2), 0, 1)
+#         last.fI = findInterval(last.ran, ProbInit.v) + 1
+#         last.fI2 = wh.nonNAs[last.fI]
+#         
+#         #                last.ran = runif(length(fI2),0,1)
+#         #                last.fI2 = wh.nonNAs[last.ran<va]
+#       }
+#       if (length(grep(pattern="Raster",class(agentlocation)))==1) {
+#         position = xyFromCell(agentlocation,fI2,spatial = T)
+#       } else if (length(grep(pattern="SpatialPoints",class(agentlocation)))==1) {
+#         position = coordinates(agentlocation)
+#       } else {
+#         stop("need raster layer or Spatial Points object")
+#       }
+#       numagents = length(position)
+#     } else {
+#       # probinit is NULL - start exactly the number of agents as there
+#       # are pixels in agentlocation
+#       if (!is.null(numagents)) {
+#         if (is(agentlocation,"Raster")) {
+#           xy=matrix(runif(numagents*2,c(xmin(ext),ymin(ext)),c(xmax(ext),ymax(ext))),ncol=2,byrow=T)
+#           colnames(xy)=c("x","y")
+#           position = SpatialPoints(xy)
+#           #                    position = SpatialPoints(sampleRandom(agentlocation, numagents, xy = T, sp = T))
+#         } else if (is(agentlocation,"SpatialPoints")) {
+#           sam = sample(1:length(agentlocation),numagents)
+#           position = SpatialPoints(agentlocation[sam,])
+#         } else {
+#           stop("need raster layer or Spatial Points object")
+#         }
+#       } else { # for numagents also NULL
+#         if (length(grep(pattern="Raster",class(agentlocation)))==1) {
+#           position = SpatialPoints(xyFromCell(agentlocation,Which(agentlocation,cells=T)))
+#         } else if (length(grep(pattern="SpatialPoints",class(agentlocation)))==1) {
+#           position = SpatialPoints(agentlocation)
+#         } else {
+#           stop("need raster layer or Spatial Points object")
+#         }
+#         numagents = length(position)
+#       }
+#     }
+#   } else if (is(agentlocation,"SpatialPolygonsDataFrame")) {
+#     if (!is.null(numagents)) {
+#       if (!is.null(pri) ) {
+#         position = SpatialPoints(dotsInPolys(agentlocation,as.integer(round(numagents*pri,0))))
+#         numagents = length(position)
+#       } else {stop("with SpatialPolygonsDataFrame, probinit is required")}
+#     } else {stop("with SpatialPolygonsDataFrame, numagents is required")}
+#   } else if (is.null(agentlocation)) { stop("Need to provide agentlocation, which can be a map layer")
+#   }
+#   heading1 = runif(numagents, 0, 360)
+#   distance = runif(numagents, 0.1, 10)
+#   
+#   .Object@ID = as.character(1:numagents)
+#   .Object@spatial = position
+#   .Object@heading = heading1
+#   .Object@distance = distance
+#   
+#   return(.Object)
+# })
