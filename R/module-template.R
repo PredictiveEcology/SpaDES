920aa57bc04b0fe0ee2f83970dfdac73c5733171
Alex Chubaty <alex.chubaty@gmail.com>
2014-07-30 10:45:38 -0700

    working on template module code

--- R/module-template.R
+++ R/module-template.R
@@ -4,72 +4,74 @@ newModule = function(name, path) {
     filename <- paste(path, name, sep="")
 
     # - need to produce code that replaces TEMPLATE with module name
-    # cat(, file=filename, fill=FALSE, sep="\n")
+    # cat(, file=filename, fill=FALSE, sep="")
 
     #@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@#
     
-    ### event functions:
-    #   - follow the naming convention `moduleName.eventType()`;
-    #   - `moduleName.init()` function is required for initiliazation;
-    #   - keep event functions short and clean, modularize by calling
-    #       subroutines from section below.
-    
-    ### template event
-    doEvent.TEMPLATE = function(sim, eventTime, eventType, debug=FALSE) {
-      if (eventType=="init") {
-        ### check for module dependencies
-        # if a required module isn't loaded yet,
-        # reschedule this module init for later
-        depends <- "NONE" # list module names here
-
-        if (reloadModuleLater(sim, depends)) {
-          sim <- scheduleEvent(sim, currentTime(sim), "habitat", "init")
-        } else {
-          sim <- habitatInit(sim)
+    cat("
+        ### event functions:
+        #   - follow the naming convention `modulenameEventtype()`;
+        #   - `modulenameInit()` function is required for initiliazation;
+        #   - module name and this filename must match;
+        #   - keep event functions short and clean, modularize by calling
+        #       subroutines from section below.
+        
+        ### template event
+        doEvent.", name, " = function(sim, eventTime, eventType, debug=FALSE) {
+          if (eventType==\"init\") {
+            ### check for module dependencies
+            # if a required module isn't loaded yet,
+            # reschedule this module init for later
+            depends <- \"NONE\" # list module names here
+            
+            if (reloadModuleLater(sim, depends)) {
+              sim <- scheduleEvent(sim, currentTime(sim), \"", name, "\", \"init\")
+            } else {
+              sim <- ", name, "Init(sim)
+            }
+          } else if (eventType==\"templateEvent\") {
+            # ! ----- EDIT BELOW ----- ! #
+            # do stuff for this event
+            
+            # schedule future event(s)
+            sim <- scheduleEvent(sim, currentTime(sim), \"", name, "\", \"init\")
+            
+            # ! ----- STOP EDITING ----- ! #
+            } else {
+              warning(paste(\"Undefined event type: \'\", simEvents(sim)[1, \"eventType\", with=FALSE],
+                            \"\' in module \'\", simEvents(sim)[1, \"moduleName\", with=FALSE], \"\'\", sep=\"\"))
+            }
+          return(sim)
         }
-      } else if (eventType=="templateEvent") {
-        # ! ----- EDIT BELOW ----- ! #
-        # do stuff for this event
-
-        # schedule future event(s)
-        sim <- scheduleEvent(sim, currentTime(sim), "habitat", "init")
-
-        # ! ----- STOP EDITING ----- ! #
-        } else {
-          warning(paste("Undefined event type: \'",simEvents(sim)[1,"eventType",with=FALSE],
-                        "\' in module \'", simEvents(sim)[1,"moduleName",with=FALSE],"\'",sep=""))
+        
+        ### template initilization
+        ", name, "Init = function(sim) {
+          # load any required packages
+          pkgs <- list(\"raster\") # list required packages here
+          loadPackforestAges(pkgs)
+          
+          # # ! ----- EDIT BELOW ----- ! #
+          
+          
+          # ! ----- STOP EDITING ----- ! #
+          
+          # last thing to do is add module name to the loaded list
+          simLoaded(sim) <- append(simLoaded(sim), \"", name, "\")
+          
+          return(sim)
         }
-      return(sim)
-    }
-
-    ### template initilization
-    templateInit = function(sim) {
-      # load any required packages
-      pkgs <- list("raster") # list required packages here
-      loadPackforestAges(pkgs)
-
-      # # ! ----- EDIT BELOW ----- ! #
-
-
-      # ! ----- STOP EDITING ----- ! #
-
-      # last thing to do is add module name to the loaded list
-      simLoaded(sim) <- append(simLoaded(sim), "TEMPLATE")
-
-      return(sim)
-    }
-
-    ### template for your module function
-    templateFunction = function(sim) {
-      # ! ----- EDIT BELOW ----- ! #
-      # do stuff for this event
-
-      # schedule future event(s)
-      sim <- scheduleEvent(sim, currentTime(sim), "TEMPLATE", "function")
-
-      # ! ----- STOP EDITING ----- ! #
-      return(sim)
-    }
-
+        
+        ### template for your module function
+        ", name, "Function = function(sim) {
+          # ! ----- EDIT BELOW ----- ! #
+          # do stuff for this event
+          
+          # schedule future event(s)
+          sim <- scheduleEvent(sim, currentTime(sim), \"", name, "\", \"function\")
+          
+          # ! ----- STOP EDITING ----- ! #
+          return(sim)
+        }",
+        file=filename, fill=FALSE, sep="")
     #@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@#
 }
