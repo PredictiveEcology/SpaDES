e192a4d13aeb352aa51b4d82512f04b2e840736a
eliotmcintire <eliotmcintire@gmail.com>
2014-08-14 17:45:28 -0700

    clean up stackFileList (capitalization of .loadFileList)

--- inst/sampleModules/stackFileList.R
+++ inst/sampleModules/stackFileList.R
@@ -5,35 +5,33 @@
 ###############################################
 
 doEvent.stackFileList <- function(sim, eventTime, eventType, debug=FALSE) {
-    if (eventType=="init") {
-        ### check for module dependencies
-        # if a required module isn't loaded yet,
-        # reschedule this module init for later if not loaded
-        depends <- "NONE" # list module names here
+  if (eventType=="init") {
+    ### check for module dependencies:
+    ### (use NULL if no dependencies exist)
+    depends <- NULL
 
-        # Check object dependencies... stack will only work on RasterLayers
-        # To Do
+    ### check for object dependencies:
+    ### (use `checkObject` or similar)
 
 
-        if (reloadModuleLater(sim, depends)) {
-            sim <- scheduleEvent(sim, currentTime(sim), "stackFileList", "init")
-        } else {
-
-          objectNames <- simObjectsLoaded(sim)
-
-          mapStack <- stack(mget(objectNames, envir=.GlobalEnv))
-          names(mapStack) <- objectNames
-
-          assign(simParams(sim)$globals$mapName, mapStack, envir=.GlobalEnv)
-
-          simPlot(mapStack, col = .cols[5:1])
-
-        }
-
+    # if a required module isn't loaded yet,
+    # reschedule this module init for later
+    if (reloadModuleLater(sim, depends)) {
+      sim <- scheduleEvent(sim, simCurrentTime(sim), "stackFileList", "init")
     } else {
-      warning(paste("Undefined event type: \'", simEvents(sim)[1,"eventType",with=FALSE],
-                    "\' in module \'", simEvents(sim)[1, "moduleName", with=FALSE] ,"\'", sep=""))
-    }
-    return(sim)
-}
+      objectNames <- simObjectsLoaded(sim)
 
+      mapStack <- stack(mget(objectNames, envir=.GlobalEnv))
+      names(mapStack) <- objectNames
+
+      assign(simParams(sim)$.globals$mapName, mapStack, envir=.GlobalEnv)
+
+      simPlot(mapStack, col = .cols[5:1])
+    }
+
+  } else {
+    warning(paste("Undefined event type: \'", simEvents(sim)[1,"eventType",with=FALSE],
+                  "\' in module \'", simEvents(sim)[1, "moduleName", with=FALSE] ,"\'", sep=""))
+  }
+  return(sim)
+}
