c63a32b7f507bfb33e95a7272bbbb2a346aa0b47
Alex Chubaty <alex.chubaty@gmail.com>
2015-01-07 15:24:34 -0800

    added documentation for package
    need to add data (maps) documentation (#115)

---
+++ R/spades-package.R
@@ -0,0 +1,26 @@
+#
+#  SpaDES/R/spades-package.R by Alex M Chubaty and Eliot J B McIntire
+#  Copyright (C) 2015
+#
+
+
+#' Develop and run spatially explicit discrete event simulation models
+#'
+#' @description
+#'
+#' Easily implement a variety of simulation models, with a focus on spatially explicit agent based models. The core simulation components are built upon a discrete event simulation framework that facilitates modularity, and easily enables the user to include additional functionality by running user-built simulation modules. Included are numerous tools to visualize raster and other maps.
+#'
+#' Bug reports: \link{http://github.com/achubaty/SpaDES/issues}
+#'
+#' @name SpaDES-package
+#' @aliases SpaDES spades
+#' @docType package
+#' @author Alex M. Chubaty \email{Alexander.Chubaty@NRCan.gc.ca} and Eliot J. B. McIntire \email{Eliot.McIntire@NRCan.gc.ca}.
+#' @keywords package
+NULL
+
+
+################################################################################
+# data documentation
+#
+
