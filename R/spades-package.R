008abc1a3feedc923b1082a9b7c674dce1559326
Alex Chubaty <alex.chubaty@gmail.com>
2015-01-07 15:48:36 -0800

    fix url in package doc description

--- R/spades-package.R
+++ R/spades-package.R
@@ -10,10 +10,10 @@
 #'
 #' Easily implement a variety of simulation models, with a focus on spatially explicit agent based models. The core simulation components are built upon a discrete event simulation framework that facilitates modularity, and easily enables the user to include additional functionality by running user-built simulation modules. Included are numerous tools to visualize raster and other maps.
 #'
-#' Bug reports: \link{http://github.com/achubaty/SpaDES/issues}
+#' Bug reports: https://github.com/achubaty/SpaDES/issues
 #'
 #' @name SpaDES-package
-#' @aliases SpaDES spades
+#' @aliases SpaDES spades-package
 #' @docType package
 #' @author Alex M. Chubaty \email{Alexander.Chubaty@@NRCan.gc.ca} and Eliot J. B. McIntire \email{Eliot.McIntire@@NRCan.gc.ca}.
 #' @keywords package
