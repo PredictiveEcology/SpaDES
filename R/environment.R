5e9cf21d94b03ada031e10a169bd78965325ad85
Alex Chubaty <alex.chubaty@gmail.com>
2015-02-28 15:42:08 -0800

    with pre (#78)

--- R/environment.R
+++ R/environment.R
@@ -5,3 +5,58 @@
 #' @rdname spadesEnv
 #'
 .spadesEnv <- new.env(parent=baseenv())
+
+#' Assign to the global environment
+#'
+#' Simple wrapper for \code{\link{assign}}.
+#'
+#' @param x   a variable name, given as a character string.
+#'            No coercion is done, and the first element of a character vector
+#'            of length greater than one will be used, with a warning.
+#'
+#' @param value The object to assign.
+#'
+#' @param ... Additional arguments to pass to \code{assign}.
+#'
+#' @export
+#' @docType methods
+#' @rdname assignGlobal-method
+#'
+#' @author Alex Chubaty
+#'
+setGeneric("assignGlobal", function(x, value, ...) {
+  standardGeneric("assignGlobal")
+})
+
+#' @rdname assignGlobal-method
+#'
+setMethod("assignGlobal",
+          signature(x="character", value="ANY"),
+          definition=function(x, value, ...) {
+            assign(x, value, envir=.GlobalEnv)
+})
+
+#' Assign to the internal SpaDES environment.
+#'
+#' Simple wrapper for \code{\link{assign}}.
+#'
+#' This is an internal function.
+#'
+#' @inheritParams assignGlobal
+#'
+#' @docType methods
+#' @rdname assignSpaDES-method
+#'
+#' @author Alex Chubaty
+#'
+setGeneric("assignSpaDES", function(x, value, ...) {
+  standardGeneric("assignSpaDES")
+})
+
+#' @rdname assignSpaDES-method
+#'
+setMethod("assignSpaDES",
+          signature(x="character", value="ANY"),
+          definition=function(x, value, ...) {
+            assign(x, value, envir=.spadesEnv)
+})
