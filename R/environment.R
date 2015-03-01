035cfc8d9dfb8426b902caff47ac7a6e4fd2bfc5
Alex Chubaty <alex.chubaty@gmail.com>
2015-02-28 16:02:24 -0800

    with prev; added `get` methods
    #78

--- R/environment.R
+++ R/environment.R
@@ -33,7 +33,36 @@ setGeneric("assignGlobal", function(x, value, ...) {
 setMethod("assignGlobal",
           signature(x="character", value="ANY"),
           definition=function(x, value, ...) {
-            assign(x, value, envir=.GlobalEnv)
+            assign(x, value, envir=.GlobalEnv, ...)
+})
+
+#' Get objects from the global environment
+#'
+#' Simple wrapper for \code{\link{get}}.
+#'
+#' @param x   a variable name, given as a character string.
+#'            No coercion is done, and the first element of a character vector
+#'            of length greater than one will be used, with a warning.
+#'
+#' @param x   For \code{getGlobal}, an object name (given as a character string).
+#'            For \code{mgetGlobal}, a character vector of object names.
+#'
+#' @export
+#' @docType methods
+#' @rdname getGlobal-method
+#'
+#' @author Alex Chubaty
+#'
+setGeneric("getGlobal", function(x, ...) {
+  standardGeneric("getGlobal")
+})
+
+#' @rdname getGlobal-method
+#'
+setMethod("getGlobal",
+          signature(x="ANY"),
+          definition=function(x, ...) {
+            get(x, envir=.GlobalEnv, ...)
 })
 
 #' Assign to the internal SpaDES environment.
@@ -58,5 +87,32 @@ setGeneric("assignSpaDES", function(x, value, ...) {
 setMethod("assignSpaDES",
           signature(x="character", value="ANY"),
           definition=function(x, value, ...) {
-            assign(x, value, envir=.spadesEnv)
+            assign(x, value, envir=.spadesEnv, ...)
+})
+
+#' Get objects from the internal SpaDES environment
+#'
+#' Simple wrapper for \code{\link{get}}.
+#'
+#' @param x   For \code{getGlobal}, an object name (given as a character string).
+#'            For \code{mgetGlobal}, a character vector of object names.
+#'
+#' @param ... Additional arguments to pass to \code{get}.
+#'
+#' @export
+#' @docType methods
+#' @rdname getSpaDES-method
+#'
+#' @author Alex Chubaty
+#'
+setGeneric("getSpaDES", function(x, ...) {
+  standardGeneric("getSpaDES")
+})
+
+#' @rdname getSpaDES-method
+#'
+setMethod("getSpaDES",
+          signature(x="ANY"),
+          definition=function(x, ...) {
+            get(x, envir=.spadesEnv, ...)
 })
