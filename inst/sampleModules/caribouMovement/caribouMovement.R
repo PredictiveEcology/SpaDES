c280e39f354d81d23c189c2b4ea798e32ee943b1
eliotmcintire <eliotmcintire@gmail.com>
2015-03-16 16:23:08 -0700

    with previous for caribouMovement randomLandscapes

--- inst/sampleModules/caribouMovement/caribouMovement.R
+++ inst/sampleModules/caribouMovement/caribouMovement.R
@@ -30,6 +30,21 @@
 ### outputObjects: objectName: simGlobals(sim)$.stackName
 ###                objectClass: RasterStack
 ###
+### caribouMovement module metadata
+defineModule(sim, list(
+  name="caribouMovement",
+  description="simulate caribou movement via correlated random walk. Requires a RasterStack object whose name is specified by `simGlobals(sim)$.stackName`, containing a RasterLayer named `habitatQuality`.",
+  keywords=c("caribou", "individual based movement model"),
+  authors=c(person(c("Eliot", "J", "B"), "McIntire", email="Eliot.McIntire@NRCan.gc.ca", role=c("aut", "cre"))),
+  version=numeric_version("0.1.0"),
+  spatialExtent=raster::extent(rep(NA_real_, 4)),
+  timeframe=as.POSIXlt(c(NA, NA)),
+  timestep=NA_real_,
+  citation=list(),
+  reqdPkgs=list("grid", "raster", "sp"),
+  inputObjects=data.frame(name=simGlobals(sim)$.stackName, class="RasterStack", stringsAsFactors=FALSE),
+  outputObjects=data.frame(name=simGlobals(sim)$.stackName, class="RasterStack", stringsAsFactors=FALSE)
+))
 
 ### event functions
 doEvent.caribouMovement <- function(sim, eventTime, eventType, debug=FALSE) {
--- inst/sampleModules/randomLandscapes/randomLandscapes.R
+++ inst/sampleModules/randomLandscapes/randomLandscapes.R
@@ -29,6 +29,22 @@
 ### outputObjects: objectName: simGlobals(sim)$.stackName
 ###                objectClass: RasterStack
 ###
+### randomLandscapes module metadata
+defineModule(sim, list(
+  name="randomLandscapes",
+  description="Generate RasterStack of random maps representative of a forest landscape (DEM, forestAge, forestCover, habitatQuality, percentPine). Requires a global simulation parameter `.stackName` be set.",
+  keywords=c("random map", "random landscape"),
+  authors=c(person(c("Alex", "M"), "Chubaty", email="Alexander.Chubaty@NRCan.gc.ca", role=c("aut", "cre")),
+            person(c("Eliot", "J", "B"), "McIntire", email="Eliot.McIntire@NRCan.gc.ca", role=c("aut", "cre"))),
+  version=numeric_version("0.1.0"),
+  spatialExtent=raster::extent(rep(NA_real_, 4)),
+  timeframe=as.POSIXlt(c(NA, NA)),
+  timestep=NA_real_,
+  citation=list(),
+  reqdPkgs=list("raster", "RColorBrewer", "tkrplot", "RandomFields"),
+  inputObjects=data.frame(name=NA_character_, class=NA_character_, stringsAsFactors=FALSE),
+  outputObjects=data.frame(name=simGlobals(sim)$.stackName, class="RasterStack", stringsAsFactors=FALSE)
+))
 
 ### event functions
 doEvent.randomLandscapes <- function(sim, eventTime, eventType, debug=FALSE) {
