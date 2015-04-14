c72e510c75079d5706510ad6dad51a4da5fe4e46
J27 <jean.d.marchal@gmail.com>
2015-04-13 17:22:36 -0700

    Following previous commit

--- tests/testthat/test-spread.R
+++ tests/testthat/test-spread.R
@@ -20,9 +20,9 @@ test_that("spread produces legal RasterLayer", {
     expect_equal(ncell(a), tabulate(spread(a, spreadProb = 1, mapID = TRUE)[]))
     
     # Several processes spreading
-    expect_equal(rep_len(50,3), tabulate(spread(a, loci=c(100, 3500, 8000), 
+    expect_equal(rep_len(3300,3), tabulate(spread(a, loci=c(100, 3500, 8000), 
                                               spreadProb = 1, mapID = TRUE,
-                                              maxSize = rep_len(50,3))[]))
+                                              maxSize = rep_len(3300,3))[]))
 
 #   expect_that(spread(a, loci=ncell(a)/2, b), is_a("RasterLayer"))
 #
