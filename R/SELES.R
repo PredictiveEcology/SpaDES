9028adb49bcc2e106780f4c69fa47928eebc67b5
eliotmcintire <eliotmcintire@gmail.com>
2015-04-14 16:44:54 -0700

    update SELES function documentation

--- R/SELES.R
+++ R/SELES.R
@@ -1,16 +1,66 @@
-### SELES options LIBRARY
+##############################################################
+#' Transitioning to next time step
+#'
+#' A SELES-like function to maintain conceptual backwards compatability with that simulation
+#' tool. Describes the probability of an agent successfully persisting until next
+#' time step. THIS IS NOT FULLY IMPLEMENTED.
+#'
+#' @param p realized probability of persisting (i.e., either 0 or 1)
+#'
+#' @param agent SpatialPoints* object
+#'
+#' @return Returns new SpatialPoints* object with potentially fewer agents
+#'
+#' @import sp
+#' @include agent.R
+#' @export
+#' @docType methods
+#' @rdname SELEStransitions
+#'
+#' @author Eliot McIntire
 transitions <- function(p, agent) {
     coordinates(agent)[which(p==0),] = NA
     return(agent)
 }
 
+##############################################################
+#' Number of Agents to initiate
+#'
+#' A SELES-like function to maintain conceptual backwards compatability with that simulation
+#' tool. Sets the the number of agents to initiate. THIS IS NOT FULLY IMPLEMENTED.
+#'
+#' @param N Number of agents to intitate
+#'
+#' @return A numeric, indicating number of agents to start
+#'
+#' @include agent.R
+#' @export
+#' @docType methods
+#' @rdname SELESnumAgents
+#'
+#' @author Eliot McIntire
 numAgents <- function(N) {
     if ((length(N) == 1) && (is.numeric(N))) numAgents = N
     else stop("N must be a single integer value, not a vector.")
     return(numAgents)
 }
 
-# this is poorly named
+##############################################################
+#' Agent Location at initiation
+#'
+#' A SELES-like function to maintain conceptual backwards compatability with that simulation
+#' tool. Sets the the location of the intiating agents.  THIS IS NOT FULLY IMPLEMENTED.
+#'
+#' @param map SpatialPoints*, SpatialPolygons* or Raster*
+#'
+#' @return Object of same class as provided as input. If a Raster*, then zeros are converted
+#' to NAs
+#'
+#' @include agent.R
+#' @export
+#' @docType methods
+#' @rdname SELESagentLocation
+#' @author Eliot McIntire
 agentLocation <- function(map) {
     if (length(grep(pattern = "Raster", class(map)))==1) {
         map[map==0] <- NA
@@ -25,17 +75,18 @@ agentLocation <- function(map) {
 }
 
 ##############################################################
-#' probInit
+#' Probability of Initiation
 #'
-#' Details here.
+#' A SELES-like function to maintain conceptual backwards compatability with that simulation
+#' tool. Describes the probability of initiation of agents or events.  THIS IS NOT FULLY IMPLEMENTED.
 #'
-#' @param map Description of this.
+#' @param map A \code{spatialObjects} object. Provides CRS and is related to probabilities.
 #'
-#' @param p Description of this.
+#' @param p probability, provided as a numeric or raster
 #'
-#' @param absolute Description of this.
+#' @param absolute logical. Is \code{p} absolute probabilities or relative?
 #'
-#' @return Decribe what it returns: fromto.
+#' @return An RasterLayer with probabilities of initialization
 #'
 #' @seealso \code{\link{print}} and \code{\link{cat}}
 #'
@@ -44,12 +95,8 @@ agentLocation <- function(map) {
 #' @include agent.R
 #' @export
 #' @docType methods
-#' @rdname probInit
-#'
+#' @rdname SELESprobInit
 #' @author Eliot McIntire
-#'
-# @examples
-# NEED EXAMPLES
 probInit = function(map, p=NULL, absolute=FALSE) {
   if (length(p) == 1) {
     probInit = raster(extent(map), nrows=nrow(map), ncols=ncol(map), crs=crs(map))
@@ -61,7 +108,7 @@ probInit = function(map, p=NULL, absolute=FALSE) {
   } else if (is(p,"NULL"))  {
     probInit = map/(cellStats(p,sum)*(1-absolute)+1*(absolute))
   } else {
-    stop("Error initializing probability map: bad inputs") # temp err msg (general)
+    stop("Error initializing probability map: bad inputs")
   }
   return(probInit)
 }
