1063b8d6220ab6208cc67e2747b9b06a54f6c673
eliotmcintire <eliotmcintire@gmail.com>
2015-05-21 14:33:46 -0700

    move simEnv definition from simList.R to environment.R
    plotting and other needs simEnv to be defined early in SpaDES package
    loading

--- R/environment.R
+++ R/environment.R
@@ -6,6 +6,104 @@
 #'
 .spadesEnv <- new.env(parent=emptyenv())
 
+#' The \code{simEnv} class
+#'
+#' This class is simply an environment that contains a \code{simList} object.
+#'
+#' @aliases simEnv
+#' @rdname simEnv-class
+#' @exportClass simEnv
+#'
+#' @author Alex Chubaty
+#'
+setClass("simEnv", contains = "environment")
+
+setMethod("initialize",
+          signature(.Object = "simEnv"),
+          definition=function(.Object) {
+            .Object$.sim <- new("simList")
+            return(.Object)
+          })
+
+### show is already defined in the methods package
+#' show simEnv
+#'
+#' @param object  Any R object
+#'
+#' @export
+setMethod("show",
+          signature="simEnv",
+          definition=function(object) {
+
+            out <- list()
+
+            ### hr
+            out[[1]] <- capture.output(cat(rep("=", getOption("width"), sep=""), "\n", sep=""))
+
+            ### simulation dependencies
+            out[[2]] <- capture.output(cat(">> Simulation dependencies:\n"))
+            out[[3]] <- "use `simDepends(sim)` to view dependencies for each module"
+            out[[4]] <- capture.output(cat("\n"))
+
+            ### simtimes
+            out[[5]] <- capture.output(cat(">> Simulation times:\n"))
+            out[[6]] <- capture.output(print(rbind(simTimes(object))))
+            out[[7]] <- capture.output(cat("\n"))
+
+            ### modules loaded
+            out[[8]] <- capture.output(cat(">> Modules:\n"))
+            out[[9]] <- capture.output(print(cbind(ModuleName=simModules(object),
+                                                   IsLoaded=simModules(object) %in%
+                                                     simModulesLoaded(object)),
+                                             quote=FALSE, row.names=FALSE))
+            out[[10]] <- capture.output(cat("\n"))
+
+            ### objects loaded
+            out[[11]] <- capture.output(cat(">> Objects Loaded:\n"))
+            out[[12]] <- capture.output(print(cbind(ObjectName=simObjectsLoaded(object)),
+                                              quote=FALSE, row.names=FALSE))
+            out[[13]] <- capture.output(cat("\n"))
+
+            ### params
+            omit <- which(names(simParams(object))==".load" |
+                            names(simParams(object))==".progress")
+
+            p <- mapply(function(x, y) {
+              data.frame(Module=x, Parameter=names(y), Value=unlist(y),
+                         stringsAsFactors=FALSE, row.names=NULL)
+            },
+            x=names(simParams(object))[-omit], y=simParams(object)[-omit],
+            USE.NAMES=TRUE, SIMPLIFY=FALSE)
+            if (length(p)) {
+              q = do.call(rbind, p)
+              q = q[order(q$Module, q$Parameter),]
+            } else {
+              q = cbind(Module=list(), Parameter=list())
+            }
+            out[[14]] <- capture.output(cat(">> Parameters:\n"))
+            out[[15]] <- capture.output(print(q, row.names=FALSE))
+            out[[16]] <- capture.output(cat("\n"))
+
+            ### completed events
+            out[[17]] <- capture.output(cat(">> Completed Events:\n"))
+            out[[18]] <- capture.output(print(simCompleted(object)))
+            out[[19]] <- capture.output(cat("\n"))
+
+            ### scheduled events
+            out[[20]] <- capture.output(cat(">> Scheduled Events:\n"))
+            out[[21]] <- capture.output(print(simEvents(object)))
+            out[[22]] <- capture.output(cat("\n"))
+
+            ### list stored objects
+            out[[23]] <- capture.output(cat(">> Objects stored:\n"))
+            out[[24]] <- capture.output(print(ls.str(object)))
+            out[[25]] <- capture.output(cat("\n"))
+
+            ### print result
+            cat(unlist(out), fill=FALSE, sep="\n")
+          })
+
+
 #' Assign to the global environment
 #'
 #' Simple wrapper for \code{\link{assign}}.
@@ -193,7 +291,7 @@ setMethod(".getSpaDES",
 #' @name .getSimEnvNames
 #' @rdname getSimEnvNames
 #'
-#' @author Eliot Mcintire
+#' @author Eliot McIntire
 #'
 setGeneric(".getSimEnvNames", function(envir){
   standardGeneric(".getSimEnvNames")
@@ -256,28 +354,57 @@ setGeneric("changeObjEnv", function(x, toEnv, fromEnv, rmSrc){
 
 #' @rdname changeObjEnv
 setMethod("changeObjEnv",
-          signature = c("character","environment", "missing", "missing"),
+          signature = c("character", "environment", "missing", "missing"),
           definition = function(x, toEnv, fromEnv, rmSrc) {
             changeObjEnv(x, toEnv, .GlobalEnv, TRUE)
 })
 
 #' @rdname changeObjEnv
 setMethod("changeObjEnv",
-          signature = c("character","environment", "missing", "logical"),
+          signature = c("character", "environment", "missing", "logical"),
           definition = function(x, toEnv, fromEnv, rmSrc) {
             changeObjEnv(x, toEnv, .GlobalEnv, rmSrc)
           })
 
 #' @rdname changeObjEnv
 setMethod("changeObjEnv",
-          signature = c("character","environment", "environment", "missing"),
+          signature = c("character", "environment", "environment", "missing"),
           definition = function(x, toEnv, fromEnv, rmSrc) {
             changeObjEnv(x, toEnv, fromEnv, rmSrc=TRUE)
           })
 
 #' @rdname changeObjEnv
 setMethod("changeObjEnv",
-          signature = c("character","environment", "environment", "logical"),
+          signature = c("character", "environment", "environment", "logical"),
+          definition = function(x, toEnv, fromEnv, rmSrc) {
+            lapply(x, function(obj) {assign(obj, envir=toEnv, value=get(obj, envir=fromEnv)); return(invisible())})
+            if(rmSrc) rm(list=x, envir=fromEnv)
+          })
+
+#' @rdname changeObjEnv
+setMethod("changeObjEnv",
+          signature = c("character", "simEnv", "missing", "missing"),
+          definition = function(x, toEnv, fromEnv, rmSrc) {
+            changeObjEnv(x, toEnv, .GlobalEnv, TRUE)
+          })
+
+#' @rdname changeObjEnv
+setMethod("changeObjEnv",
+          signature = c("character", "simEnv", "missing", "logical"),
+          definition = function(x, toEnv, fromEnv, rmSrc) {
+            changeObjEnv(x, toEnv, .GlobalEnv, rmSrc)
+          })
+
+#' @rdname changeObjEnv
+setMethod("changeObjEnv",
+          signature = c("character", "simEnv", "environment", "missing"),
+          definition = function(x, toEnv, fromEnv, rmSrc) {
+            changeObjEnv(x, toEnv, fromEnv, rmSrc=TRUE)
+          })
+
+#' @rdname changeObjEnv
+setMethod("changeObjEnv",
+          signature = c("character", "simEnv", "environment", "logical"),
           definition = function(x, toEnv, fromEnv, rmSrc) {
             lapply(x, function(obj) {assign(obj, envir=toEnv, value=get(obj, envir=fromEnv)); return(invisible())})
             if(rmSrc) rm(list=x, envir=fromEnv)
