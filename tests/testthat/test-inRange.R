f3fc05721ac8c05dde224af39d0761007f0a3876
Alex Chubaty <alex.chubaty@gmail.com>
2015-06-01 15:16:30 -0700

    update tests for v0.10.0 of `testthat`
    * use `expect_equal(a, b)` instead of `expect_that(a, equals(b))`

--- tests/testthat/test-inRange.R
+++ tests/testthat/test-inRange.R
@@ -1,11 +1,11 @@
 test_that("inRange produces logical", {
   # inputs for x
-  expect_that(inRange(0.5, 0, 1),   testthat::equals(TRUE))
-  expect_that(inRange(-0.5, 0, 1),  testthat::equals(FALSE))
-  expect_that(inRange(NA_real_),    testthat::equals(NA))
-  expect_that(inRange(NA_integer_), testthat::equals(NA))
-  expect_that(inRange(NULL),        testthat::equals(NULL))
-  expect_that(inRange(),            testthat::throws_error())
+  expect_equal(inRange(0.5, 0, 1),   TRUE)
+  expect_equal(inRange(-0.5, 0, 1),  FALSE)
+  expect_equal(inRange(NA_real_),    NA)
+  expect_equal(inRange(NA_integer_), NA)
+  expect_equal(inRange(NULL),        NULL)
+  expect_that(inRange(),             testthat::throws_error())
 
   # inputs for a & b
   expect_that(inRange(0.5, 1, 0),            testthat::throws_error())
