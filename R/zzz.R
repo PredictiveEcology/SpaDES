22c73f29252726ab05c60300a0cf3d800438cff9
Alex Chubaty <alex.chubaty@gmail.com>
2015-06-11 18:28:32 -0700

    add option `spades.nCompleted`
    new option to set the number of completed events printed when
    `debug=FALSE`; default 10

--- R/zzz.R
+++ R/zzz.R
@@ -1,11 +1,13 @@
 #' @importFrom methods loadMethod
 .onLoad <- function(libname, pkgname) {
-  options(spades.modulesRepo = "PredictiveEcology/SpaDES-modules")
   options(spades.lowMemory = FALSE)
+  options(spades.modulesRepo = "PredictiveEcology/SpaDES-modules")
+  options(spades.nCompleted = 10L)
 }
 
 .onUnload <- function(libname, pkgname) {
-  options(spades.modulesRepo = NULL)
   options(spades.lowMemory = NULL)
+  options(spades.modulesRepo = NULL)
+  options(spades.nCompleted = NULL)
 }
 
