0618c489825a68c45ae6e12c884d07e0425b1e1f
Eliot McIntire <eliot.mcintire@nrcan.gc.ca>
2015-06-13 22:52:04 -0700

    new option: spades.toleranceDigits
    the number of digits in fpCompare.tolerance. This is used in
    simCurrentTime and simStopTime, so that they are always rounded to this
    number of decimal places. This did away with my test case floating point
    problems.

--- R/zzz.R
+++ R/zzz.R
@@ -3,11 +3,13 @@
   options(spades.lowMemory = FALSE)
   options(spades.modulesRepo = "PredictiveEcology/SpaDES-modules")
   options(spades.nCompleted = 10L)
+  options(spades.toleranceDigits = -trunc(log10(getOption("fpCompare.tolerance")))+1)
 }
 
 .onUnload <- function(libname, pkgname) {
   options(spades.lowMemory = NULL)
   options(spades.modulesRepo = NULL)
   options(spades.nCompleted = NULL)
+  options(spades.toleranceDigits = NULL)
 }
 
--- _ignore/plotTesting.R
+++ _ignore/plotTesting.R
@@ -1,4 +1,4 @@
-devtools::load_all()
+devtools::load_all("~/GitHub/SpaDES")
 
 dev()
 clearPlot()
