67a94baa33cb79ecfa62a60b0d2f4d696a286e2d
Alex Chubaty <alex.chubaty@gmail.com>
2015-06-16 15:06:13 -0700

    generalized functions for creating sim. event diagrams (#184)

--- R/plotting-diagrams.R
+++ R/plotting-diagrams.R
@@ -5,11 +5,31 @@ timestep <- function(x) {
   return(as.numeric(eval(parse(text=paste0("d", x, "(1)")))))
 }
 
+moduleTimesteps <- function(sim) {
+  stopifnot(is(sim, ".simList"))
+  deps <- simDepends(sim)@dependencies
+  n <- deps %>%
+    lapply(., function(x) {
+      x@name
+    }) %>%
+    unlist
+  ts <- deps %>%
+    lapply(., function(x) {
+      x@timestepUnit
+      }) %>%
+    paste0("d", ., "(1)")
+  ts <- lapply(ts, function(x) {
+    as.numeric(eval(parse(text=x)))
+    })
+  names(ts) <- n
+  return(ts)
+}
+
 ################################################################################
-#' ganntStatus
+#' ganttStatus
 #'
 #' Internal function assign the "status" of each event to be passed to
-#' \code{\link[DiagrammeR]{mermaid}} to make a Gannt chart representing the
+#' \code{\link[DiagrammeR]{mermaid}} to make a Gantt chart representing the
 #' events in a completed simulation.
 #' 'init' events are set as "done"; 'plot' events as "critical"; and all others
 #' as "active".
@@ -23,23 +43,23 @@ timestep <- function(x) {
 #'
 #' @include simList.R
 #' @docType methods
-#' @rdname ganntStatus
+#' @rdname ganttStatus
 #'
 #' @author Alex Chubaty
 #'
-setGeneric("ganntStatus", function(eventType) {
-  standardGeneric("ganntStatus")
+setGeneric("ganttStatus", function(eventType) {
+  standardGeneric("ganttStatus")
 })
 
-#' @rdname ganntStatus
-setMethod("ganntStatus",
+#' @rdname ganttStatus
+setMethod("ganttStatus",
           signature(eventType="character"),
           definition=function(eventType) {
             status <- lapply(eventType, function(x) {
               if (x=="init") {
                 "done"
               } else if (x=="plot") {
-                "critical"
+                "crit"
               } else {
                 "active"
               }
@@ -77,14 +97,23 @@ setMethod("sim2gantt",
           signature(sim="simList", startDate="character"),
           definition=function(sim, startDate) {
             dt <- simCompleted(sim)
-            ts <- timestep(simTimestepUnit(sim)) / timestep("day")
+            ts <- timestep(simTimestepUnit(sim)) / timestep("day") # simuation timestep
 
-            out <- lapply(unique(dt$moduleName), function(x) {
+            modules <- unique(dt$moduleName)
+            mts <- lapply(moduleTimesteps(sim), '/', timestep("day")) # module timesteps
+            lapply(modules, function(x) {
+              if ( is.null(mts[[x]]) || (mts[[x]]==0) ) {
+                mts[[x]] <<- 1
+              }
+            })
+
+            out <- lapply(modules, function(x) {
               data.frame(task = dt[moduleName==x]$eventType,
                          status = ganttStatus(dt[moduleName==x]$eventType),
                          pos = paste0(x, 1:nrow(dt[moduleName==x])),
                          start = as.Date(dt[moduleName==x]$eventTime * ts, origin=startDate),
-                         end = as.Date(dt[moduleName==x]$eventTime * ts + (2*ts), origin=startDate))
+                         end = as.Date(dt[moduleName==x]$eventTime * ts + mts[[x]],
+                                       origin=startDate))
             })
             names(out) <- unique(dt$moduleName)
             return(out)
@@ -94,15 +123,24 @@ setMethod("sim2gantt",
 #' Simulation event diagram
 #'
 #' Create a Gantt Chart representing the events in a completed simulation.
+#' This event diagram is constructed using the completed event list, which by
+#' default only stores the 10 most recently completed events (unless
+#' \code{spades(debug=TRUE)} is used, in which case all events are retained).
+#' To change the number of events stored, users may override this option using
+#' \code{options(spades.nCompleted = value)}.
 #'
 #' Simulation time is presented on the x-axis, starting at date 'startDate'.
-#' Each module appears in a color-coded row, within which each event for that module
-#' is displayed corresponding to the sequence of events for that module.
+#' Each module appears in a color-coded row, within which each event for that
+#' module is displayed corresponding to the sequence of events for that module.
+#' Note that only the start time of the event is meaningful is these figures:
+#' the width of the bar associated with a particular module's event corresponds
+#' to the module's timestepUnit, not the event's "duration".
 #'
 #' Based on this StackOverflow answer: \url{http://stackoverflow.com/a/29999300/1380598}.
 #'
-#' @note A red vertical line corresponding to the current date may appear on the
-#' figure. This is useful for Gantt Charts generally but can be considered a 'bug' here.
+#' @note
+#' A red vertical line corresponding to the current date may appear on the figure.
+#' This is useful for Gantt Charts generally but can be considered a 'bug' here.
 #'
 #' @param sim  A \code{simList} object (typically corresponding to a
 #'             completed simulation).
@@ -114,7 +152,7 @@ setMethod("sim2gantt",
 #' @seealso \code{\link{mermaid}}.
 #'
 #' @include simList.R
-#' @importFrom Diagrammer mermaid
+#' @importFrom DiagrammeR mermaid
 #' @importFrom tidyr unite
 #' @export
 #' @docType methods
