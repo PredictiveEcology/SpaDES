d3dbd082b2cdf5293b3bc2032581e3f721184c66
Alex Chubaty <alex.chubaty@gmail.com>
2015-06-17 15:04:31 -0700

    additional tests for `adj` (#139)

--- tests/testthat/test-adj.R
+++ tests/testthat/test-adj.R
@@ -3,10 +3,20 @@ test_that("adj results identical to adjacent", {
 
   # smaller sample (should use matrix)
   s <- sample(1:length(a), 3)
+
+  expect_equal(sum(adj(a, s, directions=4, sort=TRUE, match.adjacent=TRUE) -
+                     adjacent(a, s, directions=4, sorted=TRUE)), 0)
+
   expect_equal(sum(adj(a, s, directions=8, sort=TRUE, match.adjacent=TRUE) -
                      adjacent(a, s, directions=8, sorted=TRUE)), 0)
 
+  expect_equal(sum(adj(a, s, directions="bishop", sort=TRUE, match.adjacent=TRUE) -
+                     adjacent(a, s, directions="bishop", sorted=TRUE)), 0)
+
   # larger sample (should use data.table)
   s <- sample(1:length(a), 1e5)
+
+  expect_equal(sum(adj(a, s, directions=4) - adjacent(a, s, directions=4)), 0)
   expect_equal(sum(adj(a, s, directions=8) - adjacent(a, s, directions=8)), 0)
+  expect_equal(sum(adj(a, s, directions="bishop") - adjacent(a, s, directions="bishop")), 0)
 })
