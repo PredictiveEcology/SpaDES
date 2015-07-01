15d3930764f2f352745d4937faaeb70cd7104a74
Alex Chubaty <alex.chubaty@gmail.com>
2015-07-01 06:39:52 -0600

    fix typo in test-mapReduce; needs test statements

--- tests/testthat/test-mapReduce.R
+++ tests/testthat/test-mapReduce.R
@@ -3,10 +3,13 @@ Ras <- raster(extent(0,15,0,15), res=1)
 fullRas <- randomPolygons(Ras, numTypes=5, speedup=1, p=0.3)
 names(fullRas) <- "mapcodeAll"
 uniqueComms <- unique(fullRas)
-reducedDT <- data.table(mapcodeAll=uniqueComms,
-   communities=sample(1:1000,length(uniqueComms)),
-   biomass=rnbinom(length(uniqueComms),mu=4000,0.4),
+reducedDT <- data.table(
+  mapcodeAll=uniqueComms,
+  communities=sample(1:1000,length(uniqueComms)),
+  biomass=rnbinom(length(uniqueComms),mu=4000,0.4)
+)
 biomass <- rasterizeReduced(reducedDT, fullRas, "biomass")
 setkey(reducedDT, biomass)
 communities <- rasterizeReduced(reducedDT, fullRas, "communities")
+# needs actual expect_xyz statements
 })
