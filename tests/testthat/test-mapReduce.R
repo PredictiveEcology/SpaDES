51cb6e050856f6306eac62402f24ac6cfa2898ac
Alex Chubaty <alex.chubaty@gmail.com>
2015-07-03 13:21:11 -0600

    fix `test-mapReduce.R`
    explicitly use `raster::unique`

--- tests/testthat/test-mapReduce.R
+++ tests/testthat/test-mapReduce.R
@@ -1,17 +1,18 @@
-test_that("mapReduced file does not work correctly", {
+test_that("mapReduce: file does not work correctly", {
+  library(data.table); on.exit(detach(package:data.table))
+  library(raster); on.exit(detach(package:raster))
 
   Ras <- raster(extent(0,15,0,15), res=1)
   fullRas <- randomPolygons(Ras, numTypes=5, speedup=1, p=0.3)
   names(fullRas) <- "mapcodeAll"
-  uniqueComms <- unique(fullRas)
+  uniqueComms <- raster::unique(fullRas)
   reducedDT <- data.table(mapcodeAll=uniqueComms,
-     communities=sample(1:1000,length(uniqueComms)),
-     biomass=rnbinom(length(uniqueComms),mu=4000,0.4))
+     communities=sample(1:1000, length(uniqueComms)),
+     biomass=rnbinom(length(uniqueComms), mu=4000, 0.4))
   biomass <- rasterizeReduced(reducedDT, fullRas, "biomass")
 
   expect_identical(sort(unique(getValues(biomass))), sort(reducedDT$biomass))
   expect_identical(length(unique(getValues(biomass))), length(unique(getValues(fullRas))))
-
 
   setkey(reducedDT, biomass)
   communities <- rasterizeReduced(reducedDT, fullRas, "communities")
