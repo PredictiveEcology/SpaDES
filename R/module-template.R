5647b9e2cc61c3a98da5bcbd50364da2db6e2608
eliotmcintire <eliotmcintire@gmail.com>
2015-07-08 15:08:15 -0700

    give a few better values for newModule - more could exist

--- R/module-template.R
+++ R/module-template.R
@@ -58,12 +58,13 @@ defineModule(sim, list(
   version=numeric_version(\"0.0.0\"),
   spatialExtent=raster::extent(rep(NA_real_, 4)),
   timeframe=as.POSIXlt(c(NA, NA)),
-  timeunit=NA_character_,
+  timeunit=NA_character_, # e.g., \"year\"
   citation=list(),
   reqdPkgs=list(),
   parameters=rbind(
-    defineParameter(\"paramName\", \"paramClass\", value, min, max, \"parameter description\"),
-    defineParameter(\"paramName\", \"paramClass\", value, min, max, \"parameter description\")),
+    defineParameter(\".plotInitialTime\", \"numeric\", NA, NA, NA, \"This describes the simulation time at which the first plot event should occur\"),
+    defineParameter(\".saveInitialTime\", \"numeric\", NA, NA, NA, \"This describes the simulation time at which the first save event should occur\")),
+    #defineParameter(\"paramName\", \"paramClass\", value, min, max, \"parameter description\")),
   inputObjects=data.frame(objectName=NA_character_, objectClass=NA_character_, other=NA_character_, stringsAsFactors=FALSE),
   outputObjects=data.frame(objectName=NA_character_, objectClass=NA_character_, other=NA_character_, stringsAsFactors=FALSE)
 ))
