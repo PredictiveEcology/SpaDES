c505140f471cd18c030e14965e58a516e0efb16b
Alex Chubaty <alex.chubaty@gmail.com>
2015-08-28 14:59:00 -0700

    more testing (#219)

--- _push_vignettes.sh
+++ _push_vignettes.sh
@@ -3,9 +3,6 @@
 ## this script takes the travis-built vignettes and pushes them to gh_pages.
 ## based on https://rmflight.github.io/posts/2014/11/travis_ci_gh_pages.html
 
-rm -rf tmp || exit 0;
-mkdir tmp
-
 GH_REPO="@github.com/PredictiveEcology/SpaDES.git"
 
 FULL_REPO="https://$GH_TOKEN$GH_REPO"
@@ -14,16 +11,13 @@ for files in '*.tar.gz'; do
   tar xfz $files
 done
 
-ls -la # temporary for debug only
+git checkout --track -b gh-pages
+git config user.name "PredictiveEcology-travis"
+git config user.email "travis"
 
-#cd PredictiveEcology/SpaDES
-#git checkout --track -b gh-pages
-#git config user.name "PredictiveEcology-travis"
-#git config user.email "travis"
+cp SpaDES/inst/doc/i-introduction.html vignettes/i-introduction.html
+cp SpaDES/inst/doc/ii-modules.html vignettes/ii-modules.html
+cp SpaDES/inst/doc/iii-plotting.html vignettes/iii-plotting.html
 
-#cp ../../SpaDES/inst/doc/i-introduction.html vignettes/i-introduction.html
-#cp ../../SpaDES/inst/doc/ii-modules.html vignettes/ii-modules.html
-#cp ../../SpaDES/inst/doc/iii-plotting.html vignettes/iii-plotting.html
-
-#git commit -m "deployed vignettes to gh-pages"
-#git push --force --quiet $FULL_REPO gh-pages
+git commit -m "deployed vignettes to gh-pages"
+git push --quiet $FULL_REPO gh-pages
