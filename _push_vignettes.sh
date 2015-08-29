8784c85cdde0320b6290de75a959dc671f684a1d
Alex Chubaty <alex.chubaty@gmail.com>
2015-08-28 19:01:20 -0700

    deal with each branch in separate dirs (#219)
    hopefully this does the trick!

--- _push_vignettes.sh
+++ _push_vignettes.sh
@@ -3,21 +3,28 @@
 ## this script takes the travis-built vignettes and pushes them to gh_pages.
 ## based on https://rmflight.github.io/posts/2014/11/travis_ci_gh_pages.html
 
+PKGDIR=$PWD
+
 GH_REPO="@github.com/PredictiveEcology/SpaDES.git"
 
 FULL_REPO="https://$GH_TOKEN$GH_REPO"
 
-for files in '*.tar.gz'; do
-  tar xfz $files
-done
+mkdir /gh-pages
+cd /gh-pages
+cp $PKGDIR/vignettes/*.Rmd .
+Rscript -e 'rmarkdown::render(".", pattern="*.Rmd")'
 
-git fetch origin gh-pages
-git checkout -t -b gh-pages origin/gh-pages
+git clone --depth=50 --branch=gh-pages $FULL_REPO SpaDES
+
+cd SpaDES
+git checkout gh-pages
 git config user.name "PredictiveEcology-travis"
 git config user.email "travis"
 
-cp SpaDES/inst/doc/*.html vignettes/
-
-git add vignettes/*.html
+cp /gh-pages/*.html SpaDES/vignettes/
+cp -r /gh-pages/*_files SpaDES/vignettes/
+git add vignettes/*
 git commit -m "deployed vignettes to gh-pages"
 git push --quiet $FULL_REPO gh-pages
+
+cd $PKGDIR
