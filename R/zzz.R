4c9fa6bacea20cca8de0e2b95e0b573c75e4d1e4
Alex Chubaty <alex.chubaty@gmail.com>
2015-11-04 11:48:00 -0800

    remove temp (incomplete) fix for #209
    R CMD check complains, so removing for now
    
    reverts changes from e677cdec61bf91f952551664e57dc0610c3d7138
    and d1f8af3558a9a1d1e4a5c8d16b868bdc53603ea2

--- R/zzz.R
+++ R/zzz.R
@@ -4,52 +4,6 @@
   options(spades.modulesRepo = "PredictiveEcology/SpaDES-modules")
   options(spades.nCompleted = 1000L)
   options(spades.tolerance = .Machine$double.eps^0.5)
-
-  ### TEMPORARY FIX FOR ISSUE #209 -- FIXES RSTUDIO BUG
-  ### https://github.com/achubaty/rstudio/commit/378ae10a29a3c6a0eb7680ff7f99d495b44aed9c
-  ### pull request to come, pending copyright transfer agreement form
-  options(editor.RStudio = getOption("editor"))
-  options(editor = function(name, file, title) {
-
-    # use internal editor for files and functions, otherwise
-    # delegate to the default editor
-    if (missing(name) || is.null(name) || is.function(name)) {
-
-      # if no name then use file
-      if (missing(name) || is.null(name)) {
-        if (!is.null(file) && nzchar(file))
-          targetFile <- file
-        else
-          targetFile <- scratchFile
-      }
-      # otherwise it's a function, write it to a file for editing
-      else {
-        functionSrc <- .rs.deparseFunction(name, TRUE, FALSE)
-        targetFile <- scratchFile
-        writeLines(functionSrc, targetFile)
-      }
-
-      # invoke the RStudio editor on the file
-      if (.Call("rs_editFile", targetFile)) {
-
-        # try to parse it back in
-        newFunc <- try(eval.parent(parse(targetFile)),
-                       silent = TRUE)
-        if (inherits(newFunc, "try-error")) {
-          stop(newFunc, "You can attempt to correct the error using ",
-               title, " = edit()")
-        }
-
-        return(newFunc)
-      }
-      else {
-        stop("Error occurred while editing function '", name, "'")
-      }
-    }
-    else
-      edit(name, file, title, editor=defaultEditor)
-  })
-  ##### END TEMPORARY FIX FOR RSTUDIO editor()
 }
 
 .onUnload <- function(libname, pkgname) {
@@ -57,9 +11,5 @@
   options(spades.modulesRepo = NULL)
   options(spades.nCompleted = NULL)
   options(spades.tolerance = NULL)
-
-  ### TEMPORARY
-  options(editor = getOption("editor.RStudio"))
-  ###
 }
 
