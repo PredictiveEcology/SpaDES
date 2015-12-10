16dc1c654cfb17a494d56b085961ae695d2266fa
Alex Chubaty <alex.chubaty@gmail.com>
2015-12-10 15:27:45 -0800

    use test module for downloadModule test

--- tests/testthat/test-downloadModule.R
+++ tests/testthat/test-downloadModule.R
@@ -8,11 +8,12 @@ test_that("downloadModule downloads and unzips module files", {
   tmpdir <- file.path(tempdir(), "modules")
   on.exit(unlink(tmpdir, recursive = TRUE))
 
-  f <- downloadModule("forestAge", tmpdir)
+  m <- "test"
+  f <- downloadModule(m, tmpdir)
 
   expect_more_than(length(f), 0)
   expect_more_than(length(file.path(tmpdir)), 0)
-  expect_more_than(length(file.path(tmpdir, "forestAge")), 0)
+  expect_more_than(length(file.path(tmpdir, m)), 0)
 })
 
 test_that("downloadData downloads and unzips module data", {
