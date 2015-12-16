d58c0ee03647fd777f86fec32dc45d88d8be3b87
Alex Chubaty <alex.chubaty@gmail.com>
2015-12-15 16:46:54 -0800

    improve downloadModule unit tests

--- tests/testthat/test-downloadModule.R
+++ tests/testthat/test-downloadModule.R
@@ -1,19 +1,48 @@
-test_that("downloadModule downloads and unzips module files", {
+test_that("downloadModule downloads and unzips a single module", {
   if (Sys.info()['sysname'] == "Windows") {
     options(download.file.method = "auto")
   } else {
     options(download.file.method = "curl")
   }
 
+  library(magrittr); on.exit(detach("package:magrittr", unload = TRUE))
+
   tmpdir <- file.path(tempdir(), "modules")
   on.exit(unlink(tmpdir, recursive = TRUE))
 
   m <- "test"
-  f <- downloadModule(m, tmpdir)
+  f <- downloadModule(m, tmpdir) %>% unlist() %>% basename()
+
+  f_expected <- c("citation.bib", "CHECKSUMS.txt", "LICENSE",
+                  "README.txt", "test.R", "test.Rmd")
 
   expect_more_than(length(f), 0)
   expect_more_than(length(file.path(tmpdir)), 0)
   expect_more_than(length(file.path(tmpdir, m)), 0)
+  expect_equal(f, f_expected)
+})
+
+test_that("downloadModule downloads and unzips a parent module", {
+  if (Sys.info()['sysname'] == "Windows") {
+    options(download.file.method = "auto")
+  } else {
+    options(download.file.method = "curl")
+  }
+
+  library(magrittr); on.exit(detach("package:magrittr", unload = TRUE))
+
+  tmpdir <- file.path(tempdir(), "modules")
+  on.exit(unlink(tmpdir, recursive = TRUE))
+
+  m <- "LCC2005"
+  f <- downloadModule(m, tmpdir) %>% unlist()
+  d <- f %>% dirname() %>% basename() %>% unique() %>% sort()
+
+  d_expected <- moduleMetadata("LCC2005", tmpdir)$childModules %>%
+    c(m, "data") %>% sort()
+
+  expect_equal(length(f), 40)
+  expect_equal(d, d_expected)
 })
 
 test_that("downloadData downloads and unzips module data", {
