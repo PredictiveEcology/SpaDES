1e7ee128074bf1fe952407f5008c0bf2feae6f39
Alex Chubaty <alex.chubaty@gmail.com>
2015-12-17 11:28:59 -0800

    minor formatting

--- tests/testthat/test-simulation.R
+++ tests/testthat/test-simulation.R
@@ -13,7 +13,7 @@ test_that("simulation runs with simInit and spades", {
   modules <- list("randomLandscapes", "caribouMovement", "fireSpread")
   paths <- list(modulePath = system.file("sampleModules", package = "SpaDES"))
 
-  mySim <- simInit(times, params, modules, objects = list(), paths) %>% spades
+  mySim <- simInit(times, params, modules, objects = list(), paths) %>% spades()
 
   # simtime
   expect_equivalent(time(mySim), 10.0)
