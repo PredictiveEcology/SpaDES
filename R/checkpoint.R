8aa51c0bf9cb1e9f970a76c7742c918b107f20b8
eliotmcintire <eliotmcintire@gmail.com>
2016-01-22 15:13:55 -0800

    clearStubArtifacts

--- R/checkpoint.R
+++ R/checkpoint.R
@@ -274,3 +274,42 @@ setMethod(
 
     simList
 })
+
+
+
+################################################################################
+#' Clear erroneous archivist artifacts
+#'
+#' When an archive object is being saved, if this is occurring at the same time
+#' as another process doing the same thing, a stub of a artifact occurs. This
+#' function will clear those stubs.
+#'
+#' @return Done for its side effect on the repoDir
+#'
+#' @param repoDir A character denoting an existing directory of the Repository for
+#' which metadata will be returned. If it is set to NULL (by default), it
+#' will use the repoDir specified in \code{archivist::setLocalRepo}.
+#'
+#' @export
+#' @importFrom archivist showLocalRepo rmFromRepo
+#' @docType methods
+#' @rdname clearStubArtifacts
+#' @author Eliot McIntire
+setGeneric("clearStubArtifacts", function(repoDir = NULL) {
+             standardGeneric("clearStubArtifacts")
+           })
+
+#' @export
+#' @rdname clearStubArtifacts
+setMethod(
+  "clearStubArtifacts",
+  definition = function(repoDir) {
+    md5hashInBackpack = showLocalRepo(repoDir=repoDir)$md5hash
+    listFiles <- dir(file.path(repoDir, "gallery")) %>% strsplit(".rda") %>% unlist()
+    toRemove <- !(md5hashInBackpack %in% listFiles)
+    md5hashInBackpack[toRemove] %>%
+      sapply(., rmFromRepo, repoDir=repoDir)
+    return(invisible(md5hashInBackpack[toRemove]))
+  }
+)
+
