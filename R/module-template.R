cfc11adf4b84621d8885d6070cabd2182e7d047b
Alex Chubaty <alex.chubaty@gmail.com>
2016-02-08 22:24:06 -0700

    cleanup/updates following PR257
    with https://github.com/PredictiveEcology/SpaDES/pull/257
    
    * spellcheck / grammar
    * improve formatting
    * update NEWS
    * redoc

--- R/module-template.R
+++ R/module-template.R
@@ -309,7 +309,7 @@ doEvent.", name, " = function(sim, eventTime, eventType, debug = FALSE) {
 ### template for your event1
 ", name, "Event1 <- function(sim) {
   # ! ----- EDIT BELOW ----- ! #
-  # THE BELOW TWO LINES ARE FOR DUMMY UNIT TESTS, CHANGE OR DELETE THEM
+  # THE NEXT TWO LINES ARE FOR DUMMY UNIT TESTS; CHANGE OR DELETE THEM.
   sim$event1Test1 <- \" this is test for event 1. \" # for dummy unit test
   sim$event1Test2 <- 999 # for dummy unit test
 
@@ -321,7 +321,7 @@ doEvent.", name, " = function(sim, eventTime, eventType, debug = FALSE) {
 ### template for your event2
 ", name, "Event2 = function(sim) {
   # ! ----- EDIT BELOW ----- ! #
-  # THE BELOW TWO LINES ARE FOR DUMMY UNIT TESTS, CHANGE OR DELETE THEM
+  # THE NEXT TWO LINES ARE FOR DUMMY UNIT TESTS; CHANGE OR DELETE THEM.
   sim$event2Test1 <- \" this is test for event 2. \" # for dummy unit test
   sim$event2Test2 <- 777  # for dummy unit test
 
@@ -551,72 +551,78 @@ test_file(\"", file.path(testthatDir, "test-template.R"), "\")\n",
 
     ## test template file
     cat("
-# please do three things when this template is corrected modified.
-# 1. rename this file based on the content you are testing, e.g., test-Event1 and Event2.R
-#    we highly suggest using test-functionName.R format to name your test file,
-#    so that your can direct call moduleCoverage to calculate module coverage information.
-#    functionName is a function's name in your module, e.g., ", name, "Event1.
-# 2. copy this file to tests folder, i.e., `", testthatDir, "`.\n
-# 3. modify the test description, i.e., test Event1 and Event2, based on the content you are testing:,
+# Please do three things to ensure this template is correctly modified:
+# 1. Rename this file based on the content you are testing using
+#    `test-functionName.R` format so that your can directly call `moduleCoverage`
+#    to calculate module coverage information.
+#    `functionName` is a function's name in your module (e.g., `", name, "Event1`).
+# 2. Copy this file to the tests folder (i.e., `", testthatDir, "`).\n
+# 3. Modify the test description based on the content you are testing:
 test_that(\"test Event1 and Event2.\", {
-module <- list(\"", name, "\")
-path <- list(modulePath = \"", path, "\", outputPath = file.path(tempdir(), \"outputs\"))
-parameters <- list(
-  #.progress = list(type = \"graphical\", interval = 1),
-  .globals = list(verbose = FALSE),
-  ", name ," = list(.saveInitialTime = NA)
-)
-times <- list(start = 0, end = 1)
+  module <- list(\"", name, "\")
+  path <- list(modulePath = \"", path, "\",
+               outputPath = file.path(tempdir(), \"outputs\"))
+  parameters <- list(
+    #.progress = list(type = \"graphical\", interval = 1),
+    .globals = list(verbose = FALSE),
+    ", name ," = list(.saveInitialTime = NA)
+  )
+  times <- list(start = 0, end = 1)
 
-# If your test function contains `time(sim)`, you can test the function at a particular simulation time by define start time above.
-object1 <- \"object1\" # please specify
-object2 <- \"object2\" # please specify
-objects <- list(\"object1\" = object1, \"object2\" = object2)
+  # If your test function contains `time(sim)`, you can test the function at a
+  # particular simulation time by defining the start time above.
+  object1 <- \"object1\" # please specify
+  object2 <- \"object2\" # please specify
+  objects <- list(\"object1\" = object1, \"object2\" = object2)
 
-mySim <- simInit(times = times,
-                 params = parameters,
-                 modules = module,
-                 objects = objects,
-                 paths = path)
+  mySim <- simInit(times = times,
+                   params = parameters,
+                   modules = module,
+                   objects = objects,
+                   paths = path)
 
-# You may need to set seed if your module or the function has the random number generator.
-set.seed(1234)
+  # You may need to set the random seed if your module or its functions use the
+  # random number generator.
+  set.seed(1234)
 
-# You have two strategies to test your module:
-# 1. test the overall simulation results for the given objects, then, use the code below:
+  # You have two strategies to test your module:
+  # 1. Test the overall simulation results for the given objects, using the
+  #    sample code below:
 
-output <- spades(mySim, debug = FALSE)
+  output <- spades(mySim, debug = FALSE)
 
-# is output a simList?
-expect_is(output, \"simList\")
+  # is output a simList?
+  expect_is(output, \"simList\")
 
-# does output have your module in it
-expect_true(any(unlist(modules(output)) %in% c(unlist(module))))
+  # does output have your module in it
+  expect_true(any(unlist(modules(output)) %in% c(unlist(module))))
 
-# did it simulate to the end?
-expect_true(time(output) == 1)
+  # did it simulate to the end?
+  expect_true(time(output) == 1)
 
-# 2. test the functions inside of the module, then, use the line below:
-# To allow the moduleCoverage function to calculate unit test coverage
-# level, it needs access to all functions directly. Use this approach
-# to when using any function within the simList object,
-# i.e., one version as a direct call, and one with simList prepended.
+  # 2. Test the functions inside of the module using the sample code below:
+  #    To allow the `moduleCoverage` function to calculate unit test coverage
+  #    level, it needs access to all functions directly.
+  #    Use this approach when using any function within the simList object
+  #    (i.e., one version as a direct call, and one with `simList` object prepended).
 
-if(exists(\"", name, "Event1\", envir = .GlobalEnv)){
-  simOutput <- ", name, "Event1(mySim)
-} else {
-  simOutput <- mySim$", name, "Event1(mySim)
-}
+  if (exists(\"", name, "Event1\", envir = .GlobalEnv)) {
+    simOutput <- ", name, "Event1(mySim)
+  } else {
+    simOutput <- mySim$", name, "Event1(mySim)
+  }
+
   expectedOutputEvent1Test1 <- \" this is test for event 1. \" # please define your expection of your output
   expect_is(class(simOutput$event1Test1), \"character\")
   expect_equal(simOutput$event1Test1, expectedOutputEvent1Test1) # or other expect function in testthat package.
   expect_equal(simOutput$event1Test2, as.numeric(999)) # or other expect function in testthat package.
 
-  if(exists(\"", name, "Event2\", envir = .GlobalEnv)){
-  simOutput <- ", name, "Event2(mySim)
+  if (exists(\"", name, "Event2\", envir = .GlobalEnv)) {
+    simOutput <- ", name, "Event2(mySim)
   } else {
-  simOutput <- mySim$", name, "Event2(mySim)
+    simOutput <- mySim$", name, "Event2(mySim)
   }
+
   expectedOutputEvent2Test1 <- \" this is test for event 2. \" # please define your expection of your output
   expect_is(class(simOutput$event2Test1), \"character\")
   expect_equal(simOutput$event2Test1, expectedOutputEvent2Test1) # or other expect function in testthat package.
