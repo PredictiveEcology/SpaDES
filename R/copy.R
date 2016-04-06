c215abd74baa5c8c186673dfa4f74991d8b0dce8
Eliot McIntire <eliotmcintire@gmail.com>
2016-04-06 13:16:05 -0700

    [copy] new fn

---
+++ R/copy.R
@@ -0,0 +1,25 @@
+### Copy a simList object
+#'
+#' Because a simList works with an environment to hold all objects,
+#' all objects within that slot are pass-by-reference. That means
+#' it is not possible to simply copy an object with an assignment operator:
+#' the two objects will share the same objects. As one simList object changes
+#' so will the other. when this is not the desired behaviour, use this function.
+#'
+#' @param sim  A \code{simList} object.
+#' @export
+#' @docType methods
+#' @rdname copy
+setGeneric("copy", function(sim) {
+  standardGeneric("copy")
+})
+
+setMethod("copy",
+          signature(sim = "simList"),
+          definition=function(sim) {
+            sim_ <- sim
+            sim_@.envir <- new.env(parent = emptyenv())
+            sim_@.envir <- list2env(mget(objects(sim), envir=sim@.envir), envir=sim_@.envir)
+            return(sim_)
+          })
+
