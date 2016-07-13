25c0cba003933674c45e42121964d208fc187356
emcintir <eliotmcintire@gmail.com>
2016-07-13 16:25:29 -0700

    [openModule] bugfix

--- R/module-template.R
+++ R/module-template.R
@@ -741,7 +741,7 @@ setMethod("openModules",
 setMethod("openModules",
           signature = c(name = "simList", path = "missing"),
           definition = function(name) {
-            mods <- unlist(modules(mySim))
+            mods <- unlist(modules(name))
             mods <- mods[-na.omit(match(mods, c("checkpoint", "save", "progress", "load")))]
             openModules(name = mods, path = modulePath(name))
           })
