43f183f1ba67b349bd200e37773847011bb30de3
Alex Chubaty <alex.chubaty@gmail.com>
2017-04-10 09:18:41 -0600

    TEMPORARY: skip spell checks on travis and appveyor
    with #262

--- tests/testthat/test-spelling.R
+++ tests/testthat/test-spelling.R
@@ -1,6 +1,8 @@
 test_that("spelling errors", {
   skip_on_cran()
   skip_if_not_installed("hunspell")
+  skip_on_appveyor() ## temporary
+  skip_on_travis()   ## temporary
 
   curDir <- getwd(); warning(curDir)
   pkgDir <- if (basename(curDir) == "testthat") {
