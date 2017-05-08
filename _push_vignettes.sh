23ac623f100df918d82360a245c8c9bb9e90bbad
Alex Chubaty <alex.chubaty@gmail.com>
2017-05-08 14:36:16 -0700

    update travis secure token

--- _push_vignettes.sh
+++ _push_vignettes.sh
@@ -15,16 +15,16 @@ done
 
 mkdir ../../gh-pages
 cd ../../gh-pages
-git clone --depth=50 --branch=gh-pages $FULL_REPO SpaDES
+git clone --depth=50 --branch=gh-pages $FULL_REPO SpaDES >/dev/null 2>&1
 
 cd SpaDES
-git checkout gh-pages
+git checkout gh-pages >/dev/null 2>&1
 git config user.name "PredictiveEcology-travis"
 git config user.email "travis"
 
 cp $PKGDIR/SpaDES/inst/doc/*.html vignettes/
 git add vignettes/*
-git commit -m "deployed vignettes to gh-pages"
-git push --quiet $FULL_REPO gh-pages
+git commit -m "deployed vignettes to gh-pages" >/dev/null 2>&1
+git push --quiet $FULL_REPO gh-pages >/dev/null 2>&1
 
 cd $PKGDIR
