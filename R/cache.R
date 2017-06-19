ab522e2d659ca1b4b7967dc02db5fb3c3d519106
emcintir <eliotmcintire@gmail.com>
2017-06-19 10:56:33 -0700

    rename makeDigestible --> robustDigest

--- R/cache.R
+++ R/cache.R
@@ -1,76 +1,45 @@
-if (!isGeneric("makeDigestible")) {
-    setGeneric("makeDigestible", function(object, objects,
+if (!isGeneric("robustDigest")) {
+    setGeneric("robustDigest", function(object, objects,
                                           compareRasterFileLength = 1e6,
                                           algo = "xxhash64") {
-    standardGeneric("makeDigestible")
+    standardGeneric("robustDigest")
   })
 }
 
-#' makeDigestible for simList class objects
+#' robustDigest for simList class objects
 #'
-#' See \code{\link[reproducible]{makeDigestible}}. This strips out stuff
+#' See \code{\link[reproducible]{robustDigest}}. This strips out stuff
 #' from a simList class object that would make it otherwise not
 #' reproducibly digestible between sessions, operating systems,
 #' or machines. This will likely still not allow identical digest
 #' results across R versions.
 #'
-#' @importFrom reproducible makeDigestible
-#' @importMethodsFrom reproducible makeDigestible
-#' @inheritParams reproducible::makeDigestible
-#' @rdname makeDigestible
+#' @importFrom reproducible robustDigest
+#' @importMethodsFrom reproducible robustDigest
+#' @inheritParams reproducible::robustDigest
+#' @rdname robustDigest
 #' @include simList-class.R
-#' @seealso \code{\link[reproducible]{makeDigestible}}
-#' @exportMethod makeDigestible
-#' @rdname makeDigestible
+#' @seealso \code{\link[reproducible]{robustDigest}}
+#' @exportMethod robustDigest
+#' @export
+#' @rdname robustDigest
 setMethod(
-  "makeDigestible",
+  "robustDigest",
   signature = "simList",
   definition = function(object, objects, compareRasterFileLength, algo) {
-    objectsToDigest <- sort(ls(object@.envir, all.names = TRUE), method = "radix")
+    allObjs <- ls(object@.envir, all.names = TRUE)
+    objectsToDigest <- sort(allObjs, method = "radix")
     if (!missing(objects)) {
       objectsToDigest <- objectsToDigest[objectsToDigest %in% objects]
     }
-    envirHash <- (sapply(objectsToDigest, function(x) {
-      if (!(x == ".sessionInfo")) {
-        obj <- get(x, envir = envir(object))
-        if (is(obj, "cluster")) {
-          dig <- NULL
-        } else {
-          if (!is(obj, "function") & !is(obj, "expression")) {
-            if (is(obj, "Raster")) {
-              # convert Rasters in the simList to some of their metadata.
-              dig <- makeDigestible(obj,
-                                    compareRasterFileLength = compareRasterFileLength,
-                                    algo = algo)
-            } else if (is(obj, "Spatial")) {
-              dig <- makeDigestible(obj, algo = algo)
-            } else {
-              if (is.character(obj)) {
-                if (any(grepl("\\/", obj))) { # test for paths
-                  obj <- basename(obj)
-                }
-              }
-              # convert functions in the simList to their digest.
-              #  functions have environments so are always unique
-              dig <- fastdigest::fastdigest(obj)
-            }
-          } else {
-            # for functions, use a character representation via format
-            dig <- fastdigest::fastdigest(format(obj))
-          }
-        }
-      } else {
-        # for .sessionInfo, just keep the major and minor R version
-        dig <- fastdigest::fastdigest(get(x, envir = envir(object))[[1]] %>%
-                                        .[c("major", "minor")])
-      }
-      return(dig)
-    }))
 
-    # Remove the NULL entries in the @.list
+    envirHash <- robustDigest(mget(objectsToDigest, envir=object@.envir))
 
-    envirHash <- envirHash[!sapply(envirHash, is.null)]
+    #envirHash <- envirHash[!sapply(envirHash, is.null)]
     envirHash <- sortDotsUnderscoreFirst(envirHash)
+    object <- Copy(object, objects = FALSE, queues = FALSE)
+    object@.envir <- new.env()
+    #rm(list = allObjs, envir=object@.envir)
 
     # Convert to a simList_ to remove the .envir slot
     object <- as(object, "simList_")
