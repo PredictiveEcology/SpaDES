134c656c4dfc12c36a23765601f10eef78d480d4
Alex Chubaty <alex.chubaty@gmail.com>
2019-10-10 10:43:57 -0600

    sticker: remove url, tone down the black

--- stickers/SpaDES-hexsticker.R
+++ stickers/SpaDES-hexsticker.R
@@ -13,8 +13,8 @@ showtext_auto()
 imgurl <- "https://github.com/PredictiveEcology/SpaDES/raw/master/docs/images/SpaDES.png"
 
 sticker(imgurl, package = "SpaDES",
-        h_color = "#5bac47", h_fill = "#000000",
-        p_color = "#cccccc", p_family = "roboto", p_size = 22, p_x = 1, p_y = 1.55,
-        s_x = 1, s_y = 1, s_width = 0.6, s_height = 0.6,
-        url = "http://spades.predictiveecology.org", u_color = "#cccccc", u_size = 4.5,
+        h_color = "#5bac47", h_fill = "#222222",
+        p_color = "#cccccc", p_family = "roboto", p_size = 22, p_x = 1, p_y = 1.5,
+        s_x = 1, s_y = 0.75, s_width = 0.4, s_height = 0.4,
+        #url = "http://spades.predictiveecology.org", u_color = "#cccccc", u_size = 4.5,
         filename = "stickers/hexsticker.png", spotlight = FALSE)
