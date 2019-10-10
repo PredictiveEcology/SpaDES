98a58d84d07be2ed42d4dde6d3851eaae21ce9a1
Alex Chubaty <alex.chubaty@gmail.com>
2019-10-10 10:35:39 -0600

    sticker path tweaks

--- stickers/SpaDES-hexsticker.R
+++ stickers/SpaDES-hexsticker.R
@@ -12,11 +12,9 @@ showtext_auto()
 
 imgurl <- "https://github.com/PredictiveEcology/SpaDES/raw/master/docs/images/SpaDES.png"
 
-dir.create("inst/figures")
-
 sticker(imgurl, package = "SpaDES",
         h_color = "#5bac47", h_fill = "#000000",
         p_color = "#cccccc", p_family = "roboto", p_size = 22, p_x = 1, p_y = 1.55,
         s_x = 1, s_y = 1, s_width = 0.6, s_height = 0.6,
         url = "http://spades.predictiveecology.org", u_color = "#cccccc", u_size = 4.5,
-        filename = "inst/figures/hexsticker.png", spotlight = FALSE)
+        filename = "stickers/hexsticker.png", spotlight = FALSE)
