fa40ff9d12b78723a2abb23b6f4039f3748181d3
Alex Chubaty <alex.chubaty@gmail.com>
2019-10-15 15:48:20 -0600

    sticker: use Teko font

--- stickers/SpaDES-hexsticker.R
+++ stickers/SpaDES-hexsticker.R
@@ -6,7 +6,7 @@ library(showtext)
 library(sysfonts)
 
 ## Loading Google fonts (http://www.google.com/fonts)
-font_add_google("Roboto Slab", "roboto")
+font_add_google("Teko", "teko")
 ## Automatically use showtext to render text for future devices
 showtext_auto()
 
@@ -14,7 +14,7 @@ imgurl <- "https://github.com/PredictiveEcology/SpaDES/raw/master/docs/images/Sp
 
 sticker(imgurl, package = "SpaDES",
         h_color = "#5bac47", h_fill = "#222222",
-        p_color = "#cccccc", p_family = "roboto", p_size = 22, p_x = 1, p_y = 1.5,
+        p_color = "#cccccc", p_family = "teko", p_size = 28, p_x = 1, p_y = 1.5,
         s_x = 1, s_y = 0.75, s_width = 0.4, s_height = 0.4,
         #url = "http://spades.predictiveecology.org", u_color = "#cccccc", u_size = 4.5,
         filename = "stickers/hexsticker.png", spotlight = FALSE)
